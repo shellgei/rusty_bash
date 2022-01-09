9c52ed822788918e2747a31f371b1910d31b892a
Ryuichi Ueda <ryuichiueda@gmail.com>
2022-01-09 13:31:57 +0900

    Add prompt

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,12 @@
 //SPDX-License-Identifier: BSD-3-Clause
 
 use std::io;
+use std::io::Write;
 
 fn main() {
+
+    print!("$ ");
+    io::stdout().flush().unwrap();
     let mut line = String::new();
     io::stdin().read_line(&mut line).expect("Failed to read line");
 
