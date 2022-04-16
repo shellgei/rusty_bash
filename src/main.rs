0d8500252b69a4f3568264b3849a7d692f02bd5d
Ryuichi Ueda <ryuichiueda@gmail.com>
2022-04-16 16:25:05 +0900

    Move codes to some small functions

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,9 @@ use std::io::Write;
 use std::process::Command;
 use std::process;
 
-use nix::unistd::{fork, ForkResult}; 
+use nix::unistd::{fork, ForkResult, Pid}; 
+use nix::sys::wait::*;
+
 fn prompt() {
     print!("$ ");
     io::stdout()
@@ -22,25 +24,51 @@ fn read_line() -> String {
     line
 }
 
-fn main() {
+fn run_ext_command(line: String) {
+    let args: Vec<&str> = line
+        .trim()
+        .split(" ")
+        .collect();
+
+    Command::new(args[0])
+        .args(&args[1..])
+        .spawn()
+        .expect("Failed to run the command.");
+}
+
+fn wait_ext_command(child: Pid) {
+    match waitpid(child, None)
+        .expect("Faild to wait child process.") {
+        WaitStatus::Exited(pid, status) => {
+            if status != 0 {
+                println!("Pid: {:?}, Exit with {:?}", pid, status);
+            }
+        }
+        WaitStatus::Signaled(pid, signal, _) => {
+            println!("Pid: {:?}, Signal: {:?}", pid, signal)
+        }
+        _ => println!("Unknown error"),
+    }
+    process::exit(0);
+}
+
+fn main_loop() {
     prompt();
     let line = read_line();
 
     match fork() {
         Ok(ForkResult::Child) => {
-            let args: Vec<&str> = line
-                .trim()
-                .split(" ")
-                .collect();
-
-            Command::new(args[0])
-                .args(&args[1..])
-                .spawn()
-                .expect("Failed to run the command");
+            run_ext_command(line)
         }
-        Ok(ForkResult::Parent { child: _, .. }) => {
-            process::exit(0);
+        Ok(ForkResult::Parent { child } ) => {
+            wait_ext_command(child)
         }
         Err(err) => panic!("Failed to fork. {}", err)
+    }
+}
+
+fn main() {
+    loop {
+        main_loop();
     }
 }
