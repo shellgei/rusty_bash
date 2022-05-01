c43a271cf05a58366b74d1f78fade097a10c89a1
Ryuichi Ueda <ryuichiueda@gmail.com>
2022-05-01 21:51:22 +0900

    Add GitHub workflow

--- test.bash
+++ test.bash
@@ -1,12 +1,19 @@
 #!/bin/bash -exv
 
+trap "echo TEST NG; exit 1" EXIT
+
 cargo build --release
 
 
 com=$(dirname $0)/target/release/bash_r
 
+### SIMPLE COMMAND TEST ###
 
 res=$($com <<< 'echo hoge')
 [ "$res" = "hoge" ]
 
-echo OK
+res=$($com <<< 'echo hoge;')
+[ "$res" = "hoge" ]
+
+trap "" EXIT
+echo TEST OK
