fe6f7537998d82d9d44f8dc72a22dff5eae71ba9
Ryuichi Ueda <ryuichiueda@gmail.com>
2022-05-07 21:06:19 +0900

    Refactor

--- src/term.rs
+++ src/term.rs
@@ -4,10 +4,11 @@
 use std::io;
 use std::io::{Write, stdout, stdin};
 use std::convert::TryInto;
+use std::io::Stdout;
 
 use termion::{event};
 use termion::cursor::DetectCursorPos;
-use termion::raw::IntoRawMode;
+use termion::raw::{IntoRawMode, RawTerminal};
 use termion::input::TermRead;
 
 pub fn prompt(text: &String) -> u16 {
@@ -24,6 +25,19 @@ fn left_cur_pos(pos: usize) -> usize {
     }else{
         pos - 1
     }
+}
+
+fn cur_move(x: u16, y: u16, stdout: &mut RawTerminal<Stdout>){
+    write!(stdout, "{}", termion::cursor::Goto(x, y)).unwrap();
+    stdout.flush().unwrap();
+}
+
+fn rewrite_line(left: u16, y: u16, text: String, stdout: &mut RawTerminal<Stdout>){
+    write!(stdout, "{}{}{}",
+           termion::cursor::Goto(left+1, y),
+           termion::clear::UntilNewline,
+           text).unwrap();
+    stdout.flush().unwrap();
 }
 
 pub fn read_line(left: u16) -> String{
@@ -44,37 +58,25 @@ pub fn read_line(left: u16) -> String{
             },
             event::Key::Left => {
                 cur_pos = left_cur_pos(cur_pos);
-                if x-widths[cur_pos] > left as u16 {
-                    write!(stdout, "{}", termion::cursor::Goto(x-widths[cur_pos], y)).unwrap();
+                if x-widths[cur_pos] > left {
+                    cur_move(x-widths[cur_pos], y, &mut stdout);
                 };
-                stdout.flush().unwrap();
             },
             event::Key::Right => {
                 if chars.len() > cur_pos+1 {
                     cur_pos += 1;
-                    write!(stdout, "{}", termion::cursor::Goto(x+widths[cur_pos], y)).unwrap();
-                    stdout.flush().unwrap();
+                    cur_move(x+widths[cur_pos], y, &mut stdout);
                 }else{
-                    write!(stdout, "{}{}{}",
-                           termion::cursor::Goto(left+1, y),
-                           termion::clear::UntilNewline,
-                           chars.iter().collect::<String>(), 
-                    ).unwrap();
-                    stdout.flush().unwrap();
+                    let line_len = widths.iter().fold(0, |line_len, w| line_len + w);
+                    cur_move(left+line_len, y, &mut stdout);
                     cur_pos = chars.len();
                 };
             },
             event::Key::Backspace => {
                 cur_pos = left_cur_pos(cur_pos);
                 chars.remove(cur_pos);
-                write!(stdout, "{}{}{}{}",
-                       termion::cursor::Goto(left+1, y),
-                       termion::clear::UntilNewline,
-                       chars.iter().collect::<String>(), 
-                       termion::cursor::Goto(x-widths[cur_pos], y))
-                    .unwrap();
-
-                stdout.flush().unwrap();
+                rewrite_line(left, y, chars.iter().collect::<String>(), &mut stdout);
+                cur_move(x-widths[cur_pos], y, &mut stdout);
             },
             event::Key::Char(c) => {
                     if c == '\n' {
@@ -86,13 +88,7 @@ pub fn read_line(left: u16) -> String{
                     cur_pos += 1;
 
                     /* output the line before the cursor */
-                    write!(stdout, "{}{}{}",
-                           termion::cursor::Goto(left+1, y),
-                           termion::clear::UntilNewline,
-                           chars[0..cur_pos].iter().collect::<String>(), 
-                    ).unwrap();
-                    stdout.flush().unwrap();
-
+                    rewrite_line(left, y, chars[0..cur_pos].iter().collect::<String>(), &mut stdout);
                     let (new_x, new_y) = stdout.cursor_pos().unwrap();
                     widths.insert(cur_pos-1, new_x - x);
 
