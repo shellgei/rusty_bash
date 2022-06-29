8da534f1924f37d5850311a12b95f5623275c2f1
Ryuichi Ueda <ryuichiueda@gmail.com>
2022-06-29 21:37:01 +0900

    Clean

--- src/abst_elems.rs
+++ src/abst_elems.rs
@@ -18,12 +18,6 @@ use crate::elem_subarg_variable::SubArgVariable;
 
 pub trait ListElem {
     fn exec(&mut self, _conf: &mut ShellCore) { }
-    fn set_pipe(&mut self, _pin: RawFd, _pout: RawFd, _pprev: RawFd) { }
-    fn get_pid(&self) -> Option<Pid> { None }
-    fn set_parent_io(&mut self) { }
-    fn get_pipe_end(&mut self) -> RawFd { -1 }
-    fn get_pipe_out(&mut self) -> RawFd { -1 }
-    fn get_eoc_string(&mut self) -> String { "".to_string() }
     fn get_text(&self) -> String;
 }
 
