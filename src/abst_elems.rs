e60a9cb5d7edf045b40a1a9ac30aaed996d6c1d9
Ryuichi Ueda <ryuichiueda@gmail.com>
2022-06-29 22:07:04 +0900

    Clean

--- src/abst_elems.rs
+++ src/abst_elems.rs
@@ -24,11 +24,10 @@ pub trait ListElem {
 pub trait PipelineElem {
     fn exec(&mut self, _conf: &mut ShellCore);
     fn set_pipe(&mut self, _pin: RawFd, _pout: RawFd, _pprev: RawFd);
-    fn get_pid(&self) -> Option<Pid> { None }
-    fn set_parent_io(&mut self) { }
-    fn get_pipe_end(&mut self) -> RawFd { -1 }
-    fn get_pipe_out(&mut self) -> RawFd { -1 }
-    fn get_eoc_string(&mut self) -> String { "".to_string() }
+    fn get_pid(&self) -> Option<Pid>;
+    fn get_pipe_end(&mut self) -> RawFd;
+    fn get_pipe_out(&mut self) -> RawFd;
+    fn get_eoc_string(&mut self) -> String;
     fn get_text(&self) -> String;
 }
 
