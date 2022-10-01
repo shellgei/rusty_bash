cae16dea8adbd80cc644d36904945f13e6c0b243
Ryuichi Ueda <ryuichiueda@gmail.com>
2022-10-01 21:48:06 +0900

    Enable to split words

--- src/elem_command.rs
+++ src/elem_command.rs
@@ -12,14 +12,14 @@ impl Command {
     pub fn exec(&mut self, _core: &mut ShellCore) { //引数_coreはまだ使いません
         if self.text == "exit\n" {
             process::exit(0);
-        }else{
-            let words: Vec<&str> = self.text
-                .trim_end()
-                .split(' ')
-                .collect();
-
-            println!("{:?}", words);
         }
+
+        let words: Vec<&str> = self.text
+            .trim_end() //末尾の改行（'\n'）を削除
+            .split(' ') //半角スペースで分割
+            .collect(); //分割したものを集めてVecに
+
+        println!("{:?}", words);
     }
 
     pub fn parse(feeder: &mut Feeder, _core: &mut ShellCore) -> Option<Command> {
