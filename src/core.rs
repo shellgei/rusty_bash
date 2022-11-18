6a39b9fc6800e80a6e757db23d35a78947706ae7
Ryuichi Ueda <ryuichiueda@gmail.com>
2022-11-18 11:53:55 +0900

    Implement wait

--- src/core.rs
+++ src/core.rs
@@ -1,6 +1,9 @@
 //SPDX-FileCopyrightText: 2022 Ryuichi Ueda ryuichiueda@gmail.com
 //SPDX-License-Identifier: BSD-3-Clause
 
+use nix::unistd::Pid;
+use nix::sys::wait::{waitpid, WaitStatus};
+
 pub struct ShellCore {
     pub history: Vec<String>,
 }
@@ -13,4 +16,22 @@ impl ShellCore {
 
         conf
     }
+
+    fn wait(child: Pid) -> i32 {
+        match waitpid(child, None) {//第2引数はオプション
+            Ok(WaitStatus::Exited(_pid, status)) => {
+                status
+            },
+            Ok(WaitStatus::Signaled(pid, signal, _)) => { //第3引数はコアダンプしたかどうか
+                eprintln!("Pid: {:?}, Signal: {:?}", pid, signal);
+                128+signal as i32 
+            },
+            Ok(unsupported) => {
+                panic!("Error: {:?}", unsupported);
+            },
+            Err(err) => {
+                panic!("Error: {:?}", err);
+            },
+        }
+    } 
 }
