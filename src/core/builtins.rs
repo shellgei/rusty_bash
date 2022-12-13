a9890373452a341a3b3c3a4a0bfbf0d728fbb668
Ryuichi Ueda <ryuichiueda@gmail.com>
2022-12-13 13:38:43 +0900

    Add functions to cd

--- src/core/builtins.rs
+++ src/core/builtins.rs
@@ -2,7 +2,7 @@
 //SPDX-License-Identifier: BSD-3-Clause
 
 use crate::ShellCore;
-use std::{env, process};
+use std::{env, fs, process};
 use std::path::Path;
 
 pub fn exit(core: &mut ShellCore, args: &mut Vec<String>) -> i32 {
@@ -22,14 +22,38 @@ pub fn exit(core: &mut ShellCore, args: &mut Vec<String>) -> i32 {
     process::exit(exit_status)
 }
 
-pub fn cd(_: &mut ShellCore, args: &mut Vec<String>) -> i32 {
-    if args.len() > 1 {
-        let path = Path::new(&args[1]);
-        if env::set_current_dir(&path).is_err() {
-            eprintln!("Cannot change directory");
-            return 1;
-        }
-
+pub fn cd(core: &mut ShellCore, args: &mut Vec<String>) -> i32 {
+    if args.len() == 0 {
+        eprintln!("Bug of this shell");
+        return 1;
     }
-    0
+    if args.len() > 2 {
+        eprintln!("{}", "bash: cd: too many arguments");
+        return 1;
+    }
+
+
+    if args.len() == 1 { //only "cd"
+        let var = env::var("HOME").expect("HOME is not defined");
+        args.push(var);
+    }else if args.len() == 2 && args[1] == "-" { // cd -
+        if let Some(old) = core.vars.get("OLDPWD") {
+            args[1] = old.to_string();
+        }
+    };
+
+    if let Ok(old) = env::current_dir() {
+        core.vars.insert("OLDPWD".to_string(), old.display().to_string());
+    };
+
+    let path = Path::new(&args[1]);
+    if env::set_current_dir(&path).is_ok() {
+        if let Ok(full) = fs::canonicalize(path) {
+            core.vars.insert("PWD".to_string(), full.display().to_string());
+        }
+        0
+    }else{
+        eprintln!("Not exist directory");
+        1
+    }
 }
