567ea0b5a1ca88aa06bf6b2609e05e0ae63e211e
Ryuichi Ueda <ryuichiueda@gmail.com>
2022-12-22 09:45:18 +0900

    Fix

--- src/elements/pipeline.rs
+++ src/elements/pipeline.rs
@@ -1,7 +1,7 @@
 //SPDX-FileCopyrightText: 2022 Ryuichi Ueda ryuichiueda@gmail.com
 //SPDX-License-Identifier: BSD-3-Clause
 
-use crate::Command;
+use crate::elem_command::Command;
 
 pub struct Pipeline {
     pub commands: Vec<Command>,
--- src/elements/script.rs
+++ src/elements/script.rs
@@ -1,7 +1,7 @@
 //SPDX-FileCopyrightText: 2022 Ryuichi Ueda ryuichiueda@gmail.com
 //SPDX-License-Identifier: BSD-3-Clause
 
-use crate::elements::pipeline::Pipeline;
+use super::pipeline::Pipeline;
 
 pub struct Script {
     pub list: Vec<Pipeline>,
