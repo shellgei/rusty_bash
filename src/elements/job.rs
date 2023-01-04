2f0021f830ce8fd0d56d5e5ae84a23b1b1efd2f2
Ryuichi Ueda <ryuichiueda@gmail.com>
2023-01-04 16:30:57 +0900

    Implement exec

--- src/elements/job.rs
+++ src/elements/job.rs
@@ -10,6 +10,12 @@ pub struct Job {
 }
 
 impl Job {
+    pub fn exec(&mut self, core: &mut ShellCore) {
+        for pipeline in self.pipelines.iter_mut() {
+            pipeline.exec(core);
+        }
+    }
+
     pub fn parse(text: &mut Feeder, core: &mut ShellCore) -> Option<Job> {
         if let Some(pipeline) = Pipeline::parse(text, core){
             return Some( Job{text: pipeline.text.clone(), pipelines: vec!(pipeline)} );
