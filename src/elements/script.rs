939b4c07671ee024a61b951429f342a37385397b
Ryuichi Ueda <ryuichiueda@gmail.com>
2023-01-07 21:07:38 +0900

    Change field name

--- src/elements/script.rs
+++ src/elements/script.rs
@@ -5,14 +5,14 @@ use super::job::Job;
 use crate::{Feeder, ShellCore};
 
 pub struct Script {
-    pub list: Vec<Job>,
+    pub jobs: Vec<Job>,
     pub text: String,
 }
 
 impl Script {
     pub fn parse(text: &mut Feeder, core: &mut ShellCore) -> Option<Script> {
         if let Some(job) = Job::parse(text, core){
-            return Some( Script{text: job.text.clone(), list: vec!(job)} );
+            return Some( Script{text: job.text.clone(), jobs: vec!(job)} );
         }
         None
     }
