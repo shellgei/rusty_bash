9a51c0243b2b37067376bbcff88935819b6030cc
Ryuichi Ueda <ryuichiueda@gmail.com>
2023-01-14 13:14:44 +0900

    Fix

--- src/elements/script.rs
+++ src/elements/script.rs
@@ -5,20 +5,20 @@ use super::job::Job;
 use crate::{Feeder, ShellCore};
 
 pub struct Script {
-    pub list: Vec<Job>,
+    pub jobs: Vec<Job>,
     pub text: String,
 }
 
 impl Script {
     pub fn exec(&mut self, core: &mut ShellCore) {
-        for job in self.list.iter_mut() {
+        for job in self.jobs.iter_mut() {
             job.exec(core);
         }
     }
 
     pub fn parse(text: &mut Feeder, core: &mut ShellCore) -> Option<Script> {
         if let Some(job) = Job::parse(text, core){
-            return Some( Script{text: job.text.clone(), list: vec!(job)} );
+            return Some( Script{text: job.text.clone(), jobs: vec!(job)} );
         }
         None
     }
