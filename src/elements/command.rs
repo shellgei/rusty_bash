e02af8539e52581903e601016f13dce3466d759c
Ryuichi Ueda <ryuichiueda@gmail.com>
2023-02-11 21:25:54 +0900

    Reduce code

--- src/elements/command.rs
+++ src/elements/command.rs
@@ -3,27 +3,9 @@
 
 pub mod simple;
 
-use crate::Feeder;
 use crate::ShellCore;
-use self::simple::SimpleCommand;
-use std::fmt;
-use std::fmt::Debug;
-
-impl Debug for dyn Command {
-    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
-        fmt.debug_struct("COMMAND").finish()
-    }
-}
 
 pub trait Command {
     fn exec(&mut self, core: &mut ShellCore);
     fn get_text(&self) -> String;
-}
-
-pub fn parse(text: &mut Feeder, core: &mut ShellCore) -> Option<Box<dyn Command>> {
-    if let Some(a) = SimpleCommand::parse(text, core){
-        Some(Box::new(a))
-    }else{
-        None
-    }
 }
