c8b5e47cb7500e9a75d87b63edf1bca2575acd01
Ryuichi Ueda <ryuichiueda@gmail.com>
2023-05-22 17:44:15 +0900

    Fix

--- src/pipe.rs
+++ src/pipe.rs
@@ -2,11 +2,16 @@
 //SPDX-License-Identifier: BSD-3-Clause
 
 use std::os::unix::prelude::RawFd;
-use nix::unistd::{close,dup2};
 
 #[derive(Debug)]
 pub struct Pipe {
     pub my_in: RawFd,
     pub my_out: RawFd,
     pub prev_out: RawFd,
+}
+
+impl Pipe {
+    pub fn new() -> Pipe {
+        Pipe{ my_in: -1, my_out: -1, prev_out: -1}
+    }
 }
