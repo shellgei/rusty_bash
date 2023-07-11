f147bf513ce5ee72b70f94cce6c23b8a31d2cf63
Ryuichi Ueda <ryuichiueda@gmail.com>
2023-07-11 12:36:54 +0900

    Move functions

--- src/elements/io.rs
+++ src/elements/io.rs
@@ -2,3 +2,20 @@
 //SPDX-License-Identifier: BSD-3-Clause
 
 pub mod pipe_recipe;
+
+use std::os::unix::prelude::RawFd;
+use nix::unistd;
+
+fn close(fd: RawFd, err_str: &str){
+    if fd >= 0 {
+        unistd::close(fd).expect(err_str);
+    }
+}
+
+fn replace(from: RawFd, to: RawFd) {
+    if from < 0 || to < 0 {
+        return;
+    }
+    unistd::dup2(from, to).expect("Can't copy file descriptors");
+    close(from, &("Can't close fd: ".to_owned() + &from.to_string()))
+}
--- src/elements/io/pipe_recipe.rs
+++ src/elements/io/pipe_recipe.rs
@@ -1,8 +1,8 @@
 //SPDX-FileCopyrightText: 2023 Ryuichi Ueda ryuichiueda@gmail.com
 //SPDX-License-Identifier: BSD-3-Clause
 
+use crate::elements::io;
 use std::os::unix::prelude::RawFd;
-use nix::unistd;
 
 #[derive(Debug)]
 pub struct PipeRecipe {
@@ -13,31 +13,17 @@ pub struct PipeRecipe {
 
 impl PipeRecipe {
     pub fn connect(&mut self) {
-        close(self.recv, "Cannot close in-pipe");
-        replace(self.send, 1);
-        replace(self.prev, 0);
+        io::close(self.recv, "Cannot close in-pipe");
+        io::replace(self.send, 1);
+        io::replace(self.prev, 0);
     }
 
     pub fn parent_close(&mut self) {
-        close(self.send, "Cannot close parent pipe out");
-        close(self.prev,"Cannot close parent prev pipe out");
+        io::close(self.send, "Cannot close parent pipe out");
+        io::close(self.prev,"Cannot close parent prev pipe out");
     }
 
     pub fn is_connected(&self) -> bool {
         self.recv != -1 || self.send != -1 || self.prev != -1
     }
-}
-
-fn close(fd: RawFd, err_str: &str){
-    if fd >= 0 {
-        unistd::close(fd).expect(err_str);
-    }
-}
-
-fn replace(from: RawFd, to: RawFd) {
-    if from < 0 || to < 0 {
-        return;
-    }
-    unistd::dup2(from, to).expect("Can't copy file descriptors");
-    close(from, &("Can't close fd: ".to_owned() + &from.to_string()))
 }
