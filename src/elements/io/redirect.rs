60cb4383912d1c660f47005565158376ceab992d
Ryuichi Ueda <ryuichiueda@gmail.com>
2023-08-17 15:56:26 +0900

    Implement parser

--- src/elements/io/redirect.rs
+++ src/elements/io/redirect.rs
@@ -6,21 +6,44 @@ use crate::{Feeder, ShellCore};
 #[derive(Debug)]
 pub struct Redirect {
     pub text: String,
+    pub symbol: String,
+    pub right: String,
 }
 
 impl Redirect {
     pub fn new() -> Redirect {
         Redirect {
             text: String::new(),
+            symbol: String::new(),
+            right: String::new(),
         }
     }
 
-    pub fn parse(feeder: &mut Feeder, _: &mut ShellCore) -> Option<Redirect> {
-        if feeder.starts_with("<file") || feeder.starts_with(">file") { //仮実装です。
-            let mut ans = Self::new();
-            ans.text = feeder.consume(5);
-            return Some(ans);
+    fn eat_symbol(feeder: &mut Feeder, ans: &mut Self) -> bool {
+        let len = feeder.scanner_redirect_symbol();
+        ans.symbol = feeder.consume(len);
+        ans.text += &ans.symbol.clone();
+        len != 0
+    }  
+
+    fn eat_right(feeder: &mut Feeder, ans: &mut Self, core: &mut ShellCore) -> bool {
+        let blank_len = feeder.scanner_blank(core);
+        ans.text += &feeder.consume(blank_len);
+
+        let len = feeder.scanner_word(core);
+        ans.right = feeder.consume(len);
+        ans.text += &ans.right.clone();
+        len != 0
+    } 
+
+    pub fn parse(feeder: &mut Feeder, core: &mut ShellCore) -> Option<Redirect> {
+        let mut ans = Self::new();
+
+        if Self::eat_symbol(feeder, &mut ans) &&
+           Self::eat_right(feeder, &mut ans, core) {
+            Some(ans)
+        }else{
+            None
         }
-        None
     }
 }
