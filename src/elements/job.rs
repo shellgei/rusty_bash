47a4c7c0bb6c15c02263f2d12c4ceb85acbd658c
Ryuichi Ueda <ryuichiueda@gmail.com>
2023-10-31 18:40:34 +0900

    Restore

--- src/elements/job.rs
+++ src/elements/job.rs
@@ -3,8 +3,6 @@
 
 use super::pipeline::Pipeline;
 use crate::{Feeder, ShellCore};
-use nix::unistd;
-use nix::unistd::Pid;
 
 #[derive(Debug)]
 pub struct Job {
@@ -14,14 +12,8 @@ pub struct Job {
 
 impl Job {
     pub fn exec(&mut self, core: &mut ShellCore) {
-        let pgid = if core.vars["$"] != core.vars["BASHPID"] {
-            unistd::getpgrp()
-        }else{
-            Pid::from_raw(0)
-        };
-
         for pipeline in self.pipelines.iter_mut() {
-            let pids = pipeline.exec(core, pgid);
+            let pids = pipeline.exec(core);
             core.wait_pipeline(pids);
         }
     }
--- src/elements/pipeline.rs
+++ src/elements/pipeline.rs
@@ -15,10 +15,10 @@ pub struct Pipeline {
 }
 
 impl Pipeline {
-    pub fn exec(&mut self, core: &mut ShellCore, pgid: Pid) -> Vec<Option<Pid>> {
+    pub fn exec(&mut self, core: &mut ShellCore) -> Vec<Option<Pid>> {
         let mut prev = -1;
         let mut pids = vec![];
-        let mut pgid = pgid;
+        let mut pgid = Pid::from_raw(0);
         for (i, p) in self.pipes.iter_mut().enumerate() {
             p.set(prev, pgid);
             pids.push(self.commands[i].exec(core, p));
