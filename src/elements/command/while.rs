a71852ba0a4777f71299a36be378936048664b40
Ryuichi Ueda <ryuichiueda@gmail.com>
2023-11-24 19:21:36 +0900

    Implement parser

--- src/elements/command/while.rs
+++ src/elements/command/while.rs
@@ -3,6 +3,7 @@
 
 use crate::{ShellCore, Feeder, Script};
 use crate::elements::Pipe;
+use crate::elements::command;
 use crate::elements::command::Command;
 use crate::elements::io::redirect::Redirect;
 use nix::unistd::Pid;
@@ -26,7 +27,36 @@ impl Command for WhileCommand {
 }
 
 impl WhileCommand {
-    pub fn parse(_: &mut Feeder, _: &mut ShellCore) -> Option<WhileCommand> {
-        None
+    fn new() -> WhileCommand {
+        WhileCommand {
+            text: String::new(),
+            while_script: None,
+            do_script: None,
+            redirects: vec![],
+            force_fork: false,
+        }
+    }
+
+    pub fn parse(feeder: &mut Feeder, core: &mut ShellCore) -> Option<WhileCommand> {
+        let mut ans = Self::new();
+        if command::eat_inner_script(feeder, core, "while", vec!["do"], &mut ans.while_script)
+        && command::eat_inner_script(feeder, core, "do", vec!["done"],  &mut ans.do_script) {
+            ans.text.push_str("while");
+            ans.text.push_str(&ans.while_script.as_mut().unwrap().text.clone());
+            ans.text.push_str("do");
+            ans.text.push_str(&ans.do_script.as_mut().unwrap().text.clone());
+            ans.text.push_str(&feeder.consume(4)); //done
+
+            loop {
+                command::eat_blank_with_comment(feeder, core, &mut ans.text);
+                if ! command::eat_redirect(feeder, core, &mut ans.redirects, &mut ans.text){
+                    break;
+                }
+            }
+
+            Some(ans)
+        }else{
+            None
+        }
     }
 }
