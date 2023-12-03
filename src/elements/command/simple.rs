48471f7917e3ab1204d290e73bf5822d024c70f9
Ryuichi Ueda <ryuichiueda@gmail.com>
2023-12-03 16:17:26 +0900

    Fix

--- src/elements/command/simple.rs
+++ src/elements/command/simple.rs
@@ -11,13 +11,6 @@ use std::process;
 use nix::unistd::{ForkResult, Pid};
 use nix::errno::Errno;
 
-fn reserved(w: &str) -> bool {
-    match w {
-        "{" | "}" | "while" | "do" | "done" => true,
-        _ => false,
-    }
-}
-
 #[derive(Debug)]
 pub struct SimpleCommand {
     pub text: String,
@@ -118,7 +111,7 @@ impl SimpleCommand {
         }
  
         let word = feeder.consume(arg_len);
-        if ans.args.len() == 0 && reserved(&word) {
+        if ans.args.len() == 0 && ( word == "{" || word == "}") {
             return false;
         }
  
