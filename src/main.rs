013f1e6878ae50c3ae898f100ae672f7412cdd1d
Ryuichi Ueda <ryuichiueda@gmail.com>
2023-12-08 14:28:19 +0900

    Add debug code

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,12 @@ mod feeder;
 mod elements;
 
 use std::{env, process, thread, time};
+use std::sync::{Arc, Mutex};
 use crate::core::ShellCore;
 use crate::elements::script::Script;
 use crate::feeder::Feeder;
 use signal_hook::consts;
+use signal_hook::iterator::Signals;
 
 fn show_version() {
     eprintln!("Sushi Shell 202305_5");
@@ -22,11 +24,31 @@ fn show_version() {
 }
 
 fn run_signal_check(core: &mut ShellCore) {
-    thread::spawn(move || loop {
-        thread::sleep(time::Duration::from_millis(1000));
-        eprint!("COME HERE");
+    let mut arc = Arc::clone(&core.signal_flags); //追加
+ 
+    thread::spawn(move || { //クロージャの処理全体を{}で囲みましょう
+        let mut signals = Signals::new(vec![consts::SIGINT])
+                          .expect("sush(fatal): cannot prepare signal data");
+
+        loop {
+            thread::sleep(time::Duration::from_millis(100)); //0.1秒周期に変更
+            for signal in signals.pending() {
+                check_signals(signal, &mut arc);
+            }
+        }
     });
 } //thanks: https://dev.to/talzvon/handling-unix-kill-signals-in-rust-55g6
+
+fn check_signals(signal: i32, arc: &mut Arc<Mutex<Vec<bool>>>) {
+    match signal {
+        consts::SIGINT => {
+            let mut flags = arc.lock().unwrap();
+            flags[consts::SIGINT as usize] = true;
+            eprintln!("\nCOME HERE\n"); //確認用
+        },
+        _ => {},
+    }
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
