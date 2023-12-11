83f51f0bb5179056012d4ac5fc21b411cbf4c665
Ryuichi Ueda <ryuichiueda@gmail.com>
2023-12-11 16:35:07 +0900

    Fix

--- src/core.rs
+++ src/core.rs
@@ -22,7 +22,7 @@ pub struct ShellCore {
     pub vars: HashMap<String, String>,
     pub builtins: HashMap<String, fn(&mut ShellCore, &mut Vec<String>) -> i32>,
     pub nest: Vec<(String, Vec<String>)>,
-    pub sigint: Arc<AtomicBool>, //pub input_interrupt: bool,
+    pub sigint: Arc<AtomicBool>,
     pub is_subshell: bool,
     pub tty_fd: RawFd,
     pub job_table: Vec<JobEntry>,
