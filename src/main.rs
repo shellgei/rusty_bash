4e3a0a2b2cc5a040b3bb916c0a0bfcf33e077c3b
Ryuichi Ueda <ryuichiueda@gmail.com>
2023-12-12 20:44:03 +0900

    Add fd hack

--- src/main.rs
+++ src/main.rs
@@ -25,11 +25,20 @@ fn show_version() {
 }
 
 fn run_signal_check(core: &mut ShellCore) {
+    for fd in 3..10 { //use FD 3~9 to prevent signal-hool from using these FDs
+        nix::unistd::dup2(2, fd).expect("sush(fatal): init error");
+    }
+
     let sigint = Arc::clone(&core.sigint); //追加
  
     thread::spawn(move || { //クロージャの処理全体を{}で囲みましょう
         let mut signals = Signals::new(vec![consts::SIGINT])
                           .expect("sush(fatal): cannot prepare signal data");
+
+        for fd in 3..10 { // release FD 3~9
+            nix::unistd::close(fd).expect("sush(fatal): init error");
+        }
+
         loop {
             thread::sleep(time::Duration::from_millis(100)); //0.1秒周期に変更
             for signal in signals.pending() {
@@ -40,6 +49,7 @@ fn run_signal_check(core: &mut ShellCore) {
             }
         }
     });
+
 } //thanks: https://dev.to/talzvon/handling-unix-kill-signals-in-rust-55g6
 
 fn main() {
