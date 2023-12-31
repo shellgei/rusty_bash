472da6fcc2d21ed317a7f9c52b885959e2ff704a
Ryuichi Ueda <ryuichiueda@gmail.com>
2024-01-01 00:21:12 +0900

    Refactor

--- src/core/builtins/utils.rs
+++ src/core/builtins/utils.rs
@@ -8,25 +8,27 @@ use std::path::{Path, PathBuf, Component};
 pub fn make_absolute_path(core: &mut ShellCore, path_str: &str) -> PathBuf {
     let path = Path::new(&path_str);
     let mut absolute = PathBuf::new();
-    if path.is_relative() {
-        if path.starts_with("~") { // tilde -> $HOME
-            if let Some(home_dir) = core.vars.get("HOME") {
-                absolute.push(PathBuf::from(home_dir));
-                if path_str.len() > 1 && path_str.starts_with("~/") {
-                    absolute.push(PathBuf::from(&path_str[2..]));
-                } else {
-                    absolute.push(PathBuf::from(&path_str[1..]));
-                }
-            }
-        } else { // current
-            if let Some(tcwd) = &core.get_current_directory() {
-                absolute.push(tcwd);
-                absolute.push(path);
-            };
-        }
-    } else {
+    if ! path.is_relative() {
         absolute.push(path);
+        return absolute;
     }
+
+    if path.starts_with("~") { // tilde -> $HOME
+        if let Some(home_dir) = core.vars.get("HOME") {
+            absolute.push(PathBuf::from(home_dir));
+            if path_str.len() > 1 && path_str.starts_with("~/") {
+                absolute.push(PathBuf::from(&path_str[2..]));
+            } else {
+                absolute.push(PathBuf::from(&path_str[1..]));
+            }
+        }
+    } else { // current
+        if let Some(tcwd) = &core.get_current_directory() {
+            absolute.push(tcwd);
+            absolute.push(path);
+        };
+    }
+
     absolute
 }
 
