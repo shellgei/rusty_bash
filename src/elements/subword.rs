da7ee9533f957b339005d45200a5e39173e147e8
Ryuichi Ueda <ryuichiueda@gmail.com>
2024-02-03 10:10:58 +0900

    Add trait Subword

---
+++ src/elements/subword.rs
@@ -0,0 +1,15 @@
+//SPDX-FileCopyrightText: 2024 Ryuichi Ueda ryuichiueda@gmail.com
+//SPDX-License-Identifier: BSD-3-Clause
+
+use std::fmt;
+use std::fmt::Debug;
+
+impl Debug for dyn Subword {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        fmt.debug_struct(&self.get_text()).finish()
+    }
+}
+
+pub trait Subword {
+    fn get_text(&self) -> &str;
+}
