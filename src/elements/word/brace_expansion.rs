97379b52ab68f197b4bc1f5b43dabd72e87ba1c3
Ryuichi Ueda <ryuichiueda@gmail.com>
2024-02-04 15:08:45 +0900

    Change order

--- src/elements/word/brace_expansion.rs
+++ src/elements/word/brace_expansion.rs
@@ -5,10 +5,7 @@ use crate::elements::subword::Subword;
 use crate::elements::word::Word;
 
 pub fn eval(word: &mut Word) -> Vec<Word> {
-    let mut start_pos = vec![];
-    for sw in word.subwords.iter().enumerate().filter(|e| e.1.get_text() == "{") {
-        start_pos.push(sw.0);
-    }
+    let mut start_pos = open_brace_pos(word);
     if word.text.starts_with("{}") {
         start_pos.remove(0);
     }
@@ -23,11 +20,13 @@ pub fn eval(word: &mut Word) -> Vec<Word> {
     vec![word.clone()]
 }
 
-pub fn parse(_: &[Box<dyn Subword>]) -> Option<Vec<usize>> {
-    None
+fn open_brace_pos(w: &Word) -> Vec<usize> {
+    w.subwords.iter()
+        .enumerate()
+        .filter(|e| e.1.get_text() == "{")
+        .map(|e| e.0)
+        .collect()
 }
 
-pub fn expand(_: &Vec<Box<dyn Subword>>, _: &Vec<usize>) -> Vec<Word> {
-    vec![]
-}
-
+fn parse(_: &[Box<dyn Subword>]) -> Option<Vec<usize>> { None }
+fn expand(_: &Vec<Box<dyn Subword>>, _: &Vec<usize>) -> Vec<Word> { vec![] }
