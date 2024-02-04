e9dd86d2faefe4c0b4b3904c3ac5a4a5fcf2a8c1
Ryuichi Ueda <ryuichiueda@gmail.com>
2024-02-04 23:00:23 +0900

    Reduce code

--- src/elements/word/brace_expansion.rs
+++ src/elements/word/brace_expansion.rs
@@ -5,10 +5,7 @@ use crate::elements::subword::Subword;
 use crate::elements::word::Word;
 
 pub fn eval(word: &mut Word) -> Vec<Word> {
-    let mut start_pos = open_brace_pos(word);
-    if word.text.starts_with("{}") {
-        start_pos.remove(0);
-    }
+    let start_pos = open_brace_pos(word);
 
     for i in start_pos {
         if let Some(d) = parse(&word.subwords[i..]) {
