715709eb3bc9217b7d8f6741fe12fb7a8cf407f9
Ryuichi Ueda <ryuichiueda@gmail.com>
2024-02-15 16:39:46 +0900

    Adjust for the article

--- src/elements/word.rs
+++ src/elements/word.rs
@@ -1,6 +1,8 @@
 //SPDX-FileCopyrightText: 2024 Ryuichi Ueda <ryuichiueda@gmail.com>
 //SPDX-License-Identifier: BSD-3-Clause
 
+use crate::{Feeder, ShellCore};
+use crate::elements::subword;
 use crate::elements::subword::Subword;
 
 #[derive(Debug, Clone)] //Cloneも指定しておく
@@ -12,5 +14,34 @@ pub struct Word {
 impl Word {
     pub fn eval(&mut self) -> Vec<String> {
         vec![self.text.clone()]
+    }
+
+    pub fn new() -> Word {
+        Word {
+            text: String::new(),
+            subwords: vec![],
+        }
+    }
+
+    fn push(&mut self, subword: &Box<dyn Subword>) {
+        self.text += &subword.get_text().to_string();
+        self.subwords.push(subword.clone()); //図4で作ったcloneを使用
+    }
+
+    pub fn parse(feeder: &mut Feeder, core: &mut ShellCore) -> Option<Word> {
+        if feeder.starts_with("#") {
+            return None;
+        }
+
+        let mut ans = Word::new();
+        while let Some(sw) = subword::parse(feeder, core) {
+            ans.push(&sw);
+        }
+
+        if ans.text.len() == 0 {
+            None
+        }else{
+            Some(ans)
+        }
     }
 }
