1bcb9dd73e97f7e7e59702a33f0ece6f07f6bb16
Ryuichi Ueda <ryuichiueda@gmail.com>
2024-02-19 19:03:31 +0900

    Rewind code

--- src/elements/word/parameter_expansion.rs
+++ src/elements/word/parameter_expansion.rs
@@ -6,16 +6,14 @@ use crate::elements::word::Word;
 use crate::elements::subword::SubwordType;
 
 pub fn eval(word: &mut Word, core: &mut ShellCore) {
-    for i in word.scan_pos("$") {
-        for j in i+1..word.subwords.len() {
-            if word.subwords[j].get_type() != SubwordType::VarName {
-                break;
-            }
-
-            let right = word.subwords[j].clone();
-            word.subwords[i].merge(SubwordType::Parameter, &right);
-            word.subwords[j].clear();
+    eprint!("parse of {}: ", &word.text);
+    for sw in &word.subwords {
+        if sw.get_type() == SubwordType::VarName {
+            eprint!("NAME");
+        }else{
+            eprint!("{}", sw.get_text());
         }
     }
+    eprintln!("");
     word.subwords.iter_mut().for_each(|w| w.parameter_expansion(core));
 }
