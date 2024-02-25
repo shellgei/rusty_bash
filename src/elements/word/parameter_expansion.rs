f8849a70d575b6ae9e57772c110842836060f43f
Ryuichi Ueda <ryuichiueda@gmail.com>
2024-02-25 14:49:55 +0900

    Refactor

--- src/elements/word/parameter_expansion.rs
+++ src/elements/word/parameter_expansion.rs
@@ -7,9 +7,13 @@ use crate::elements::subword::{Subword, SubwordType};
 
 pub fn eval(word: &mut Word, core: &mut ShellCore) {
     for i in word.scan_pos("$") {
-        replace(&mut word.subwords[i..], core);
+        replace_variables(&mut word.subwords[i..], core);
     }
 
+    replace_parameters(word, core);
+}
+
+fn replace_parameters(word: &mut Word, core: &mut ShellCore) {
     for s in &mut word.subwords {
         if s.get_type() == SubwordType::Parameter {
             let text = s.get_text();
@@ -19,7 +23,7 @@ pub fn eval(word: &mut Word, core: &mut ShellCore) {
     }
 }
 
-fn replace(subwords: &mut [Box<dyn Subword>], core: &mut ShellCore) {
+fn replace_variables(subwords: &mut [Box<dyn Subword>], core: &mut ShellCore) {
     let mut text = String::new();
     let mut pos = 1;
     for s in &mut subwords[1..] {
