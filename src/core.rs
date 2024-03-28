3dddbfc36d9736f2a4947e8b7412ca57401b3e16
Ryuichi Ueda <ryuichiueda@gmail.com>
2024-03-28 17:30:35 +0900

    Add Terminal

--- src/core.rs
+++ src/core.rs
@@ -1,20 +1,26 @@
 //SPDX-FileCopyrightText: 2024 Ryuichi Ueda ryuichiueda@gmail.com
 //SPDX-License-Identifier: BSD-3-Clause
 
+mod terminal;
+
 use nix::unistd;
+use self::terminal::Terminal;
 
 pub struct ShellCore {
     flags: String,
+    term: Option<Terminal>,
 }
 
 impl ShellCore {
     pub fn new() -> ShellCore {
         let mut core = ShellCore {
             flags: String::new(),
+            term: None,
         };
 
         if unistd::isatty(0) == Ok(true) {
             core.flags += "i";
+            core.term = Some(Terminal::new());
         }
 
         core
