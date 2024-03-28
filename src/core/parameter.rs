4803c2f79df8eb3983e374f755db0e9f5ee7c1e4
Ryuichi Ueda <ryuichiueda@gmail.com>
2024-03-29 08:33:35 +0900

    Add comment

--- src/core/parameter.rs
+++ src/core/parameter.rs
@@ -5,20 +5,20 @@ use crate::ShellCore;
 use std::env;
 
 impl ShellCore {
-    pub fn get_param_ref(&mut self, key: &str) -> &str {
-        if  self.parameters.get(key) == None {
-            if let Ok(val) = env::var(key) {
-                self.set_param(key, &val);
+    pub fn get_param_ref(&mut self, key: &str) -> &str { //パラメータの参照を返す
+        if  self.parameters.get(key) == None { //キーがない場合
+            if let Ok(val) = env::var(key) {   //環境変数がないか探す
+                self.set_param(key, &val);     //環境変数があったら内部のパラメータとして取り込む
             }
         }
 
-        match self.parameters.get(key) {
-            Some(val) => val,
-            None      => "",
+        match self.parameters.get(key) { //改めてキーに対応する値を探す
+            Some(val) => val,            //あれば文字列の参照を返す
+            None      => "",             //なければ空文字を返す
         }
     }
 
-    pub fn set_param(&mut self, key: &str, val: &str) {
+    pub fn set_param(&mut self, key: &str, val: &str) { //値のセット
         self.parameters.insert(key.to_string(), val.to_string());
     }
 }
