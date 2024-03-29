57eca21838c1ae188f5993e0b6947cd094bb710c
Ryuichi Ueda <ryuichiueda@gmail.com>
2024-03-29 09:56:42 +0900

    Simplify

--- src/feeder.rs
+++ src/feeder.rs
@@ -34,7 +34,7 @@ impl Feeder {
 
     pub fn feed_line(&mut self, core: &mut ShellCore) -> Result<(), InputError> {
         let line = match self.term.as_mut() {
-            Some(t) => {let _ = t.read_line_normal(core); Self::read_line_stdin()},
+            Some(t) => {t.show_prompt(core, "PS1"); Self::read_line_stdin()},
             _ => Self::read_line_stdin(),
         };
 
--- src/terminal.rs
+++ src/terminal.rs
@@ -1,7 +1,7 @@
 //SPDX-FileCopyrightText: 2024 Ryuichi Ueda ryuichiueda@gmail.com
 //SPDX-License-Identifier: BSD-3-Clause
 
-use crate::{InputError, ShellCore};
+use crate::ShellCore;
 use std::io;
 use std::io::Write;
 use unicode_width::UnicodeWidthStr;
@@ -13,16 +13,11 @@ impl Terminal {
         Terminal { }
     }
 
-    fn show_prompt(&mut self, core: &mut ShellCore, mode: &str) -> usize {
+    pub fn show_prompt(&mut self, core: &mut ShellCore, mode: &str) -> usize {
         let prompt = core.get_param_ref(mode);
         print!("{} ", prompt);
         io::stdout().flush().unwrap();
         UnicodeWidthStr::width(prompt)
-    }
-
-    pub fn read_line_normal(&mut self, core: &mut ShellCore) -> Result<String, InputError> {
-        self.show_prompt(core, "PS1");
-        Ok(String::new())
     }
 }
 
