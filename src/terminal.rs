a576ea84fd236ebcdf76abc7babcb559a071c654
Ryuichi Ueda <ryuichiueda@gmail.com>
2024-03-29 09:46:03 +0900

    Simplify

--- src/terminal.rs
+++ src/terminal.rs
@@ -6,26 +6,22 @@ use std::io;
 use std::io::Write;
 use unicode_width::UnicodeWidthStr;
 
-pub struct Terminal {
-}
+pub struct Terminal { }
 
 impl Terminal {
     pub fn new() -> Terminal {
-        Terminal {
-        }
+        Terminal { }
     }
 
-    fn prompt_normal(&mut self, core: &mut ShellCore) -> usize {
-        let prompt = core.get_param_ref("PS1");
-    
+    fn show_prompt(&mut self, core: &mut ShellCore, mode: &str) -> usize {
+        let prompt = core.get_param_ref(mode);
         print!("{} ", prompt);
         io::stdout().flush().unwrap();
-    
-        UnicodeWidthStr::width(prompt) + 1
+        UnicodeWidthStr::width(prompt)
     }
 
     pub fn read_line_normal(&mut self, core: &mut ShellCore) -> Result<String, InputError> {
-        self.prompt_normal(core);
+        self.show_prompt(core, "PS1");
         Ok(String::new())
     }
 }
