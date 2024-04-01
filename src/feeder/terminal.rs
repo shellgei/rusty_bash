e7d1f32bc4d1a9b8695d1f4bd45cfdb00027829c
Ryuichi Ueda <ryuichiueda@gmail.com>
2024-04-01 17:22:38 +0900

    Fix

--- src/feeder/terminal.rs
+++ src/feeder/terminal.rs
@@ -127,7 +127,6 @@ pub fn read_line(core: &mut ShellCore, prompt: &str) -> Result<String, InputErro
 
     for c in io::stdin().keys() {
         term.check_size_change(&mut term_size);
-
         match c.as_ref().unwrap() {
             event::Key::Ctrl('a') => term.goto_origin(),
             event::Key::Ctrl('c') => {
