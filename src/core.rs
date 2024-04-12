2c39a1f84bd7b0fff6f808cf0cbf8a76e7b0752c
Ryuichi Ueda <ryuichiueda@gmail.com>
2024-04-12 17:53:01 +0900

    Fix

--- src/core.rs
+++ src/core.rs
@@ -25,7 +25,7 @@ pub struct ShellCore {
     pub nest: Vec<(String, Vec<String>)>,
     pub sigint: Arc<AtomicBool>,
     pub is_subshell: bool,
-    pub tty_fd: RawFd,
+    pub tty_fd: Option<OwnedFd>,
     pub job_table: Vec<JobEntry>,
     tcwd: Option<path::PathBuf>, // the_current_working_directory
 }
@@ -57,7 +57,7 @@ impl ShellCore {
             nest: vec![("".to_string(), vec![])],
             sigint: Arc::new(AtomicBool::new(false)),
             is_subshell: false,
-            tty_fd: -1,
+            tty_fd: None,
             job_table: vec![],
             tcwd: None,
         };
@@ -68,8 +68,9 @@ impl ShellCore {
 
         if is_interactive() {
             core.flags += "i";
-            core.tty_fd = fcntl::fcntl(2, fcntl::F_DUPFD_CLOEXEC(255))
+            let fd = fcntl::fcntl(2, fcntl::F_DUPFD_CLOEXEC(255))
                 .expect("sush(fatal): Can't allocate fd for tty FD");
+            core.tty_fd = Some(unsafe{OwnedFd::from_raw_fd(fd)});
         }
 
         core
@@ -115,14 +116,22 @@ impl ShellCore {
     } 
 
     fn set_foreground(&self) {
-        if self.tty_fd < 0 { // tty_fdが無効なら何もしない
+        /*
+        if self.tty_fd == None { // tty_fdが無効なら何もしない
             return;
+        }*/
+
+        match self.tty_fd.as_ref() {
+            Some(fd) => {
+                ignore_signal(Signal::SIGTTOU); //SIGTTOUを無視
+                unistd::tcsetpgrp(fd, unistd::getpid())
+                    .expect("sush(fatal): cannot get the terminal");
+                restore_signal(Signal::SIGTTOU); //SIGTTOUを受け付け
+            },
+            _ => {},
         }
 
-        ignore_signal(Signal::SIGTTOU); //SIGTTOUを無視
-        unistd::tcsetpgrp(unsafe{OwnedFd::from_raw_fd(self.tty_fd)}, unistd::getpid())
-            .expect("sush(fatal): cannot get the terminal");
-        restore_signal(Signal::SIGTTOU); //SIGTTOUを受け付け
+
     }
 
     pub fn wait_pipeline(&mut self, pids: Vec<Option<Pid>>) {
