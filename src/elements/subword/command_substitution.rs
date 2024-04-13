4ef6858590a98c69556b63e04c333d5398eb22a2
Ryuichi Ueda <ryuichiueda@gmail.com>
2024-04-13 22:53:18 +0900

    Change to paren

--- src/elements/subword/command_substitution.rs
+++ src/elements/subword/command_substitution.rs
@@ -1,14 +1,17 @@
 //SPDX-FileCopyrightText: 2024 Ryuichi Ueda ryuichiueda@gmail.com
 //SPDX-License-Identifier: BSD-3-Clause
 
-use crate::{Script, ShellCore, Feeder};
-use crate::elements::command;
+use crate::{ShellCore, Feeder};
+use crate::elements::Pipe;
+use crate::elements::command::Command;
+use crate::elements::command::paren::ParenCommand;
 use crate::elements::subword::{Subword, SubwordType};
 
 #[derive(Debug, Clone)]
 pub struct CommandSubstitution {
     pub text: String,
-    script: Option<Script>,
+    command: Option<ParenCommand>,
+    pipe: Pipe,
 }
 
 impl Subword for CommandSubstitution {
@@ -16,7 +19,7 @@ impl Subword for CommandSubstitution {
     fn boxed_clone(&self) -> Box<dyn Subword> {Box::new(self.clone())}
 
     fn substitution(&mut self, core: &mut ShellCore) -> bool {
-        match self.script {
+        match self.command {
             Some(_) => {
                 self.text = self.exec(core);
                 true
@@ -33,20 +36,28 @@ impl CommandSubstitution {
     fn new() -> CommandSubstitution {
         CommandSubstitution {
             text: String::new(),
-            script: None,
+            command: None,
+            pipe: Pipe::new("|".to_string()),
         }
     }
 
     fn exec(&mut self, core: &mut ShellCore) -> String {
+        match self.command.as_mut() {
+            Some(c) => {c.exec(core, &mut self.pipe);},
+            _ => {},
+        }
         "ok".to_string()
     }
 
     pub fn parse(feeder: &mut Feeder, core: &mut ShellCore) -> Option<Self> {
-        let mut ans = Self::new();
-        if command::eat_inner_script(feeder, core, "$(", vec![")"], &mut ans.script) {
-            ans.text.push_str("$(");
-            ans.text.push_str(&ans.script.as_ref().unwrap().get_text());
-            ans.text.push_str(&feeder.consume(1));
+        if ! feeder.starts_with("$(") {
+            return None;
+        }
+        let mut ans = CommandSubstitution::new();
+        ans.text = feeder.consume(1);
+
+        if let Some(pc) = ParenCommand::parse(feeder, core) {
+            ans.command = Some(pc);
             Some(ans)
         }else{
             None
