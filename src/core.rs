cda3f552f001481270df897b2cbc1ddb2e675e35
Ryuichi Ueda <ryuichiueda@gmail.com>
2024-04-28 21:59:33 +0900

    Add branch display

--- src/core.rs
+++ src/core.rs
@@ -25,7 +25,7 @@ impl ShellCore {
 
         if unistd::isatty(0) == Ok(true) {
             core.flags += "i";
-            core.set_param("PS1", r"\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;35m\]\w\[\033[00m\]🍣 ");
+            core.set_param("PS1", r"\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;35m\]\w\[\033[00m\]🍣\[\033[01;36m\]\b\[\033[00m\] ");
             core.set_param("PS2", "> ");
         }
 
