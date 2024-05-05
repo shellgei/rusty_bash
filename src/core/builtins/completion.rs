f226602a7589c2d250b1b972d783ca5f9a5f7056
Ryuichi Ueda <ryuichiueda@gmail.com>
2024-05-05 09:43:43 +0900

    Implement compgen -f

--- src/core/builtins/completion.rs
+++ src/core/builtins/completion.rs
@@ -3,6 +3,43 @@
 
 use crate::{ShellCore, Feeder};
 use crate::elements::word::Word;
+use faccess;
+use faccess::PathExt;
+use std::path::PathBuf;
+use glob;
+use glob::{GlobError, MatchOptions};
+
+fn expand(path: &str, executable_only: bool, search_dir: bool) -> Vec<String> {
+    let opts = MatchOptions {
+        case_sensitive: true,
+        require_literal_separator: true,
+        require_literal_leading_dot: false,
+    };
+
+    match glob::glob_with(&path, opts) {
+        Ok(ps) => ps.map(|p| to_str(&p, executable_only, search_dir))
+                    .filter(|s| s != "").collect(),
+        _ => vec![],
+    }
+}
+
+fn to_str(path :&Result<PathBuf, GlobError>, executable_only: bool, search_dir: bool) -> String {
+    match path {
+        Ok(p) => {
+            if ( executable_only && ! p.executable() && ! p.is_dir() )
+            || ( ! search_dir && p.is_dir() ) {
+                return "".to_string();
+            }
+
+            let mut s = p.to_string_lossy().to_string();
+            if p.is_dir() && s.chars().last() != Some('/') {
+                s.push('/');
+            }
+            s
+        },
+        _ => "".to_string(),
+    }
+}
 
 pub fn compgen(core: &mut ShellCore, args: &mut Vec<String>) -> i32 {
     if args.len() <= 1 {
@@ -10,12 +47,30 @@ pub fn compgen(core: &mut ShellCore, args: &mut Vec<String>) -> i32 {
     }
 
     match args[1].as_str() {
+        "-f" => compgen_f(core, args),
         "-W" => compgen_large_w(core, args),
         _ => {
             eprintln!("sush: compgen: {}: invalid option", &args[1]);
             return 2;
         },
     }
+}
+
+pub fn compgen_f(core: &mut ShellCore, args: &mut Vec<String>) -> i32 {
+    let mut path = match args.len() {
+        2 => "*".to_string(),
+        _ => args[2].to_string() + "*",
+    };
+
+    if path.starts_with("~/") {
+        let home = core.data.get_param_ref("HOME").to_string() + "/";
+        path = path.replace("~/", &home);
+    }
+
+    let mut paths = expand(&path, false, true);
+    paths.iter_mut().for_each(|p| if p.ends_with("/") { p.pop(); });
+    paths.iter().for_each(|a| println!("{}", a));
+    0
 }
 
 pub fn compgen_large_w(core: &mut ShellCore, args: &mut Vec<String>) -> i32 {
