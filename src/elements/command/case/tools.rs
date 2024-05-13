5e4e89bac514e01fdc2b075090001e20de65b581
Ryuichi Ueda <ryuichiueda@gmail.com>
2024-05-13 18:28:00 +0900

    Add a skeleton of parser

--- src/elements/command/case/tools.rs
+++ src/elements/command/case/tools.rs
@@ -8,7 +8,48 @@ pub fn compare(word: &String, pattern: &str) -> bool {
 }
 
 fn to_regex(pattern: &str) -> Regex {
-    let re = Regex::new(&(r"^".to_owned() + pattern + "$")).unwrap();
+    let mut regex_str = String::new();
+    let mut remaining = pattern.to_string();
+
+    while remaining.len() > 0 {
+        let len = scanner_escaped_char(&remaining);
+        if len > 0 {
+            regex_str += &consume(&mut remaining, len);
+            continue;
+        }
+
+        let len = scanner_char(&remaining);
+        if len > 0 {
+            regex_str += &consume(&mut remaining, len);
+        }
+    }
+
+    let re = Regex::new(&(r"^".to_owned() + &regex_str + "$")).unwrap();
 
     re
+}
+
+fn scanner_escaped_char(remaining: &str) -> usize {
+    if ! remaining.starts_with("\\") {
+        return 0;
+    }
+
+    match remaining.chars().nth(1) {
+        None    => 1,
+        Some(c) => 1 + c.len_utf8(),
+    }
+}
+
+fn scanner_char(remaining: &str) -> usize {
+    match remaining.chars().nth(0) {
+        None    => 0,
+        Some(c) => c.len_utf8(),
+    }
+}
+
+fn consume(remaining: &mut String, cutpos: usize) -> String {
+    let cut = remaining[0..cutpos].to_string();
+    *remaining = remaining[cutpos..].to_string();
+
+    cut
 }
