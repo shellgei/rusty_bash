925c474e56e4e232cc0282bd844a25a9353a0071
Ryuichi Ueda <ryuichiueda@gmail.com>
2024-05-13 18:06:21 +0900

    Enable to use regex

--- src/elements/command/case/tools.rs
+++ src/elements/command/case/tools.rs
@@ -1,6 +1,14 @@
 //SPDX-FileCopyrightText: 2024 Ryuichi Ueda <ryuichiueda@gmail.com>
 //SPDX-License-Identifier: BSD-3-Clause
 
-pub fn compare(word: &String, pattern: &String) -> bool {
-    word == pattern
+use regex::Regex;
+
+pub fn compare(word: &String, pattern: &str) -> bool {
+    to_regex(pattern).is_match(word)
+}
+
+fn to_regex(pattern: &str) -> Regex {
+    let re = Regex::new(&(r"^".to_owned() + pattern + "$")).unwrap();
+
+    re
 }
