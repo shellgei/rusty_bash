4ba699936ef74d1d1bc78dd8413d01365514d635
Ryuichi Ueda <ryuichiueda@gmail.com>
2024-05-14 16:11:50 +0900

    Rewrite wildcard match

--- src/elements/command/case/tools.rs
+++ src/elements/command/case/tools.rs
@@ -1,48 +1,105 @@
 //SPDX-FileCopyrightText: 2024 Ryuichi Ueda <ryuichiueda@gmail.com>
 //SPDX-License-Identifier: BSD-3-Clause
 
-use regex::Regex;
-
-pub fn compare(word: &String, pattern: &str) -> bool {
-    to_regex(pattern).is_match(word)
+#[derive(Debug)]
+enum Wildcard {
+    Normal(String),
+    Asterisk,
+    Question,
+    //OneOf(Vec<char>),
+    //NotOneOf(Vec<char>),
 }
 
-fn to_regex(pattern: &str) -> Regex {
-    let mut regex_str = String::new();
+pub fn compare(word: &String, pattern: &str) -> bool {
+    let wildcards = parse(pattern);
+    let mut candidates = vec![word.to_string()];
+
+    for w in wildcards {
+        match w {
+            Wildcard::Normal(s) => compare_normal(&mut candidates, &s),
+            Wildcard::Asterisk  => asterisk(&mut candidates),
+            Wildcard::Question  => question(&mut candidates),
+        }
+    }
+
+    candidates.len() > 0
+}
+
+pub fn compare_normal(cands: &mut Vec<String>, s: &String) {
+    let mut ans = vec![];
+
+    for c in cands.into_iter() {
+        if ! c.starts_with(s) {
+            continue;
+        }
+        
+        ans.push(c[s.len()..].to_string());
+    }
+
+    *cands = ans;
+}
+
+pub fn asterisk(cands: &mut Vec<String>) {
+    let mut ans = vec![];
+    for cand in cands.into_iter() {
+        let mut s = String::new();
+        ans.push(s.clone());
+        for c in cand.chars().rev() {
+            s = c.to_string() + &s.clone();
+            ans.push(s.clone());
+        }
+    }
+
+    *cands = ans;
+}
+
+pub fn question(cands: &mut Vec<String>) {
+    let mut ans = vec![];
+    for cand in cands.into_iter() {
+        match cand.chars().nth(0) {
+            Some(c) => {
+                let len = c.len_utf8();
+                ans.push(cand[len..].to_string());
+            },
+            _ => {},
+        }
+    }
+    *cands = ans;
+}
+
+fn parse(pattern: &str) -> Vec<Wildcard > {
+    let pattern = pattern.to_string();
     let mut remaining = pattern.to_string();
+
+    let mut ans = vec![];
 
     while remaining.len() > 0 {
         match scanner_escaped_char(&remaining) {
             0 => {}, 
             len => {
-                let mut ans = consume(&mut remaining, len);
-                ans.remove(0);
-                regex_str += &ans;
+                let mut s = consume(&mut remaining, len);
+                s.remove(0);
+                ans.push( Wildcard::Normal(s) );
                 continue;
             },
         }
 
         if remaining.starts_with("*") {
             consume(&mut remaining, 1);
-            regex_str += ".*";
+            ans.push( Wildcard::Asterisk );
             continue;
         }else if remaining.starts_with("?") {
             consume(&mut remaining, 1);
-            regex_str += ".";
-            continue;
-        }else if remaining.starts_with(".") {
-            consume(&mut remaining, 1);
-            regex_str += "\\.";
+            ans.push( Wildcard::Question );
             continue;
         }
 
-        let len = scanner_char(&remaining);
-        regex_str += &consume(&mut remaining, len);
+        let len = scanner_chars(&remaining);
+        let s = consume(&mut remaining, len);
+        ans.push( Wildcard::Normal(s) );
     }
 
-    let re = Regex::new(&(r"^".to_owned() + &regex_str + "$")).unwrap();
-
-    re
+    ans
 }
 
 fn scanner_escaped_char(remaining: &str) -> usize {
@@ -56,11 +113,16 @@ fn scanner_escaped_char(remaining: &str) -> usize {
     }
 }
 
-fn scanner_char(remaining: &str) -> usize {
-    match remaining.chars().nth(0) {
-        None    => 0,
-        Some(c) => c.len_utf8(),
+fn scanner_chars(remaining: &str) -> usize {
+    let mut ans = 0;
+    for c in remaining.chars() {
+        if c == '*' || c == '?' {
+            return ans;
+        }
+
+        ans += c.len_utf8();
     }
+    ans
 }
 
 fn consume(remaining: &mut String, cutpos: usize) -> String {
