e3ab4a3fbaca640ecf96d5bf5cd1396a32227bee
Ryuichi Ueda <ryuichiueda@gmail.com>
2024-05-18 11:34:24 +0900

    Fix

--- src/elements/command/if.rs
+++ src/elements/command/if.rs
@@ -2,8 +2,7 @@
 //SPDX-License-Identifier: BSD-3-Clause
 
 use crate::{ShellCore, Feeder, Script};
-use nix::unistd::Pid;
-use super::{Command, Pipe, Redirect};
+use super::{Command, Redirect};
 
 #[derive(Debug)]
 pub struct IfCommand {
@@ -16,11 +15,11 @@ pub struct IfCommand {
 }
 
 impl Command for IfCommand {
-    fn exec(&mut self, _: &mut ShellCore, _: &mut Pipe) -> Option<Pid> {None}
     fn run(&mut self, _: &mut ShellCore, _: bool) {}
     fn get_text(&self) -> String { self.text.clone() }
     fn get_redirects(&mut self) -> &mut Vec<Redirect> { &mut self.redirects }
     fn set_force_fork(&mut self) { self.force_fork = true; }
+    fn force_fork(&self) -> bool { self.force_fork }
 }
 
 impl IfCommand {
