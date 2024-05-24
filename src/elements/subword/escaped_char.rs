f2a25ad9c16b6cfbb184bc9bcdf9260c620ec99a
Ryuichi Ueda <ryuichiueda@gmail.com>
2024-05-25 08:38:07 +0900

    Clean

--- src/elements/subword/escaped_char.rs
+++ src/elements/subword/escaped_char.rs
@@ -12,34 +12,9 @@ pub struct EscapedChar {
 impl Subword for EscapedChar {
     fn get_text(&self) -> &str {&self.text.as_ref()}
     fn boxed_clone(&self) -> Box<dyn Subword> {Box::new(self.clone())}
-
-    fn merge(&mut self, right: &Box<dyn Subword>) {
-        self.text += &right.get_text();
-    }
-
-    /*
-    fn set(&mut self, subword_type: SubwordType, s: &str){
-        self.text = s.to_string();
-    }*/
-
-    /*
-    fn substitute(&mut self, core: &mut ShellCore) -> bool {
-        match self.subword_type {
-            SubwordType::Parameter => {
-                let value = core.data.get_param(&self.text[1..]);
-                self.text = value.to_string();
-            },
-            _ => {},
-        }
-        true
-    }*/
-
+    fn merge(&mut self, right: &Box<dyn Subword>) { self.text += &right.get_text(); }
     fn make_glob_string(&mut self) -> String { self.text.clone() }
-
-    fn make_unquoted_string(&mut self) -> String {
-        self.text[1..].to_string()
-    }
-
+    fn make_unquoted_string(&mut self) -> String { self.text[1..].to_string() }
     fn get_type(&self) -> SubwordType { SubwordType::EscapedChar  }
     fn clear(&mut self) { self.text = String::new(); }
 }
