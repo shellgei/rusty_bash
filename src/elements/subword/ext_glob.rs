b14edfb7592fc7c594e484f58bf398a851668aab
Ryuichi Ueda <ryuichiueda@gmail.com>
2024-05-25 14:01:33 +0900

    Fix

--- src/elements/subword/ext_glob.rs
+++ src/elements/subword/ext_glob.rs
@@ -15,34 +15,6 @@ pub struct ExtGlob {
 impl Subword for ExtGlob {
     fn get_text(&self) -> &str {&self.text.as_ref()}
     fn boxed_clone(&self) -> Box<dyn Subword> {Box::new(self.clone())}
-
-    fn substitute(&mut self, core: &mut ShellCore) -> bool {
-        let mut word = Word::new();
-        word.subwords = self.subwords.to_vec();
-        if ! substitution::eval(&mut word, core) {
-            return false;
-        }
-        self.subwords = word.subwords;
-        self.text = self.subwords.iter().map(|s| s.get_text()).collect();
-        true
-    }
-
-    fn make_glob_string(&mut self) -> String {
-        return self.text.replace("\\", "\\\\")
-                        .replace("*", "\\*")
-                        .replace("?", "\\?")
-                        .replace("[", "\\[")
-                        .replace("]", "\\]");
-    }
-
-    fn make_unquoted_string(&mut self) -> String {
-        self.subwords.iter_mut()
-            .map(|s| s.make_unquoted_string())
-            .collect::<Vec<String>>()
-            .concat()
-    }
-
-    fn is_quoted(&self) -> bool {true}
 }
 
 impl ExtGlob {
