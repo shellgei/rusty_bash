591fbc46e604ae6a925b34e97ae53ba104c9d70d
Ryuichi Ueda <ryuichiueda@gmail.com>
2024-05-27 21:19:17 +0900

    Implement set command with no arg

--- src/core/builtins/set.rs
+++ src/core/builtins/set.rs
@@ -2,12 +2,33 @@
 //SPDX-License-Identifier: BSD-3-Clause
 
 use crate::ShellCore;
+use std::collections::HashMap;
 
-pub fn set(core: &mut ShellCore, args: &mut Vec<String>) -> i32 {
+fn set_no_arg(core: &mut ShellCore) -> i32 {
+    let mut output = HashMap::new();
+    for layer in &core.data.parameters {
+        layer.iter().for_each(|e| {output.insert(e.0, e.1); } );
+    }
+
+    for e in output {
+        println!("{}={}", e.0, e.1); 
+    }
+    0
+}
+
+pub fn set_parameters(core: &mut ShellCore, args: &mut Vec<String>) -> i32 {
     match core.data.position_parameters.pop() {
         None => panic!("SUSH INTERNAL ERROR: empty param stack"),
         _    => {},
     }
     core.data.position_parameters.push(args.to_vec());
     0
+}
+
+pub fn set(core: &mut ShellCore, args: &mut Vec<String>) -> i32 {
+    match args.len() {
+        0 => panic!("never come here"),
+        1 => set_no_arg(core),
+        _ => set_parameters(core, args),
+    }
 }
