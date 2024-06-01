fe60e7ceba7424147767752bac9da9ca751b5ca6
Ryuichi Ueda <ryuichiueda@gmail.com>
2024-06-01 17:44:45 +0900

    Fix

--- src/elements/word.rs
+++ src/elements/word.rs
@@ -1,7 +1,7 @@
 //SPDX-FileCopyrightText: 2024 Ryuichi Ueda <ryuichiueda@gmail.com>
 //SPDX-License-Identifier: BSD-3-Clause
 
-use crate::elements::subword::Subword;
+use super::subword::Subword;
 
 #[derive(Debug, Clone)] //Cloneも指定しておく
 pub struct Word {
