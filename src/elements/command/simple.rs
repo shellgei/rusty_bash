3c11d1ecc7327cc8327517d771dd7709004caf70
Ryuichi Ueda <ryuichiueda@gmail.com>
2024-06-03 19:21:39 +0900

    Clean

--- src/elements/command/simple.rs
+++ src/elements/command/simple.rs
@@ -36,14 +36,13 @@ impl Command for SimpleCommand {
             return None;
         }
 
-        self.args.clear();
-        let mut words = self.words.to_vec();
-
         if ! self.eval_substitutions(core){
             core.data.set_param("?", "1");
             return None;
         }
 
+        self.args.clear();
+        let mut words = self.words.to_vec();
         if ! words.iter_mut().all(|w| self.set_arg(w, core)){
             return None;
         }
