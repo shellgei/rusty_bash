fecda4389590b0958c65328e3f10ff4d4f4ffc8d
Ryuichi Ueda <ryuichiueda@gmail.com>
2024-06-07 18:09:38 +0900

    Use custom tokenizer for completion

--- src/utils.rs
+++ src/utils.rs
@@ -4,3 +4,52 @@ pub fn reserved(w: &str) -> bool {
         _ => false,
     }
 }
+
+pub fn split_words(s: &str) -> Vec<String> {
+    let mut ans = vec![];
+
+    let mut in_quote = false;
+    let mut escaped = false;
+    let mut quote = ' ';
+
+    let mut tmp = String::new();
+    for c in s.chars() {
+        if escaped || c == '\\' {
+            escaped = ! escaped;
+            tmp.push(c);
+            continue;
+        }
+
+        if c == '\'' || c == '"' {
+            if c == quote {
+                in_quote = ! in_quote;
+                quote = ' ';
+            }else if quote == ' ' {
+                in_quote = ! in_quote;
+                quote = c;
+            }
+            tmp.push(c);
+            continue;
+        }
+
+        if in_quote {
+            tmp.push(c);
+            continue;
+        }
+
+        if ! in_quote && ( c == ' ' || c == '\t') {
+            if tmp.len() != 0 {
+                ans.push(tmp.clone());
+                tmp.clear();
+            }
+        }else{
+            tmp.push(c);
+        }
+    }
+
+    if tmp.len() != 0 {
+        ans.push(tmp);
+    }
+
+    ans
+}
