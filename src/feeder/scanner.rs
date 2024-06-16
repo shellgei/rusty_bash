bca6f582c7250d4a9ac948a3e1de5ae89d06fe7a
Ryuichi Ueda <ryuichiueda@gmail.com>
2024-06-16 17:19:46 +0900

    Fix

--- src/feeder/scanner.rs
+++ src/feeder/scanner.rs
@@ -78,29 +78,20 @@ impl Feeder {
     }
 
     pub fn scanner_job_end(&mut self) -> usize {
-        if let Some(ch) = self.remaining.chars().nth(0) {
-            if let Some(_) = ";&\n".find(ch) {
-                return 1;
-            }
-        }
-        0
+        self.scanner_one_of(&[";", "&", "\n"])
     }
 
     pub fn scanner_and_or(&mut self, core: &mut ShellCore) -> usize {
         self.backslash_check_and_feed(vec!["|", "&"], core);
-
-        if self.remaining.starts_with("||")     { 2 }
-        else if self.remaining.starts_with("&&"){ 2 }
-        else{ 0 }
+        self.scanner_one_of(&["||", "&&"])
     }
 
     pub fn scanner_pipe(&mut self, core: &mut ShellCore) -> usize {
         self.backslash_check_and_feed(vec!["|"], core);
-
-        if self.remaining.starts_with("||")     { 0 }
-        else if self.remaining.starts_with("|&"){ 2 }
-        else if self.remaining.starts_with("|") { 1 }
-        else{ 0 }
+        if self.starts_with("||") {
+            return 0;
+        }
+        self.scanner_one_of(&["|&","|"])
     }
 
     pub fn scanner_comment(&self) -> usize {
@@ -110,7 +101,7 @@ impl Feeder {
 
         let mut ans = 0;
         for ch in self.remaining.chars() {
-            if let Some(_) = "\n".find(ch) {
+            if "\n".find(ch) != None {
                 break;
             }
             ans += ch.len_utf8();
@@ -120,11 +111,6 @@ impl Feeder {
 
     pub fn scanner_redirect_symbol(&mut self, core: &mut ShellCore) -> usize {
         self.backslash_check_and_feed(vec![">", "&"], core);
-
-        if self.remaining.starts_with("&>")     { 2 } //追加
-        else if self.remaining.starts_with(">>"){ 2 }
-        else if self.remaining.starts_with("<"){ 1 }
-        else if self.remaining.starts_with(">"){ 1 }
-        else{ 0 }
+        self.scanner_one_of(&["&>", ">&", ">>", "<", ">"])
     }
 }
