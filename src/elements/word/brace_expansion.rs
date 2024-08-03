20763f12ed18d7ca21284dd5f4d4bdb4840dccd4
Ryuichi Ueda <ryuichiueda@gmail.com>
2024-08-03 14:45:54 +0900

    Reduce

--- src/elements/word/brace_expansion.rs
+++ src/elements/word/brace_expansion.rs
@@ -6,7 +6,8 @@ use super::Word;
 
 pub fn eval(word: &mut Word) -> Vec<Word> {
     for i in open_brace_pos(word) {
-        if let Some(d) = parse(&word.subwords[i..], i) {
+        let d = parse(&word.subwords[i..], i);
+        if d.len() > 2 {
             return expand(&word.subwords, &d);
         }
     }
@@ -21,37 +22,33 @@ fn open_brace_pos(w: &Word) -> Vec<usize> {
         .collect()
 }
 
-pub fn parse(subwords: &[Box<dyn Subword>], start: usize) -> Option<Vec<usize>> {
+pub fn parse(subwords: &[Box<dyn Subword>], start: usize) -> Vec<usize> {
     let mut stack = vec![];
     for sw in subwords {
         stack.push(sw.get_text());
         if sw.get_text() == "}" {
-            match get_delimiters(&mut stack, start) {
-                Some(ds) => return Some(ds),
-                _        => {},
+            let ds = get_delimiters(&mut stack, start);
+            if ds.len() > 2 {
+                return ds;
             }
         }
     }
-    None
+    vec![]
 }
 
-fn get_delimiters(stack: &mut Vec<&str>, start: usize) -> Option<Vec<usize>> {
+fn get_delimiters(stack: &mut Vec<&str>, start: usize) -> Vec<usize> {
     let mut delimiter_pos = vec![start, stack.len()-1+start];
     for i in (1..stack.len()-1).rev() {
         match stack[i] {
             "," => delimiter_pos.insert(1, start+i),
             "{" => { // find an inner brace expdelimiter_posion
                 stack[i..].iter_mut().for_each(|e| *e = "");
-                return None;
+                return vec![];
             },
             _   => {},
         }
     }
-
-    match delimiter_pos.len() {
-        2 => None,
-        _ => Some(delimiter_pos),
-    }
+    delimiter_pos
 }
 
 pub fn expand(subwords: &Vec<Box<dyn Subword>>, delimiters: &Vec<usize>) -> Vec<Word> {
