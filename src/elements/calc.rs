7cfc96f31189f14cfc4a618c0270725a3e27488b
Ryuichi Ueda <ryuichiueda@gmail.com>
2024-08-04 15:30:27 +0900

    Clean

--- src/elements/calc.rs
+++ src/elements/calc.rs
@@ -31,18 +31,9 @@ fn syntax_error_msg(token: &str) -> String {
     format!("{0}: syntax error: operand expected (error token is \"{0}\")", token)
 }
 
-fn recursion_error(token: &str) -> String {
-    format!("{0}: expression recursion level exceeded (error token is \"{0}\")", token)
-}
-
-fn is_name(s: &str, core: &mut ShellCore) -> bool {
-    let mut f = Feeder::new(s);
-    s.len() > 0 && f.scanner_name(core) == s.len()
-}
-
 impl Calc {
     pub fn eval(&mut self, core: &mut ShellCore) -> Option<String> {
-        let es = match self.words_to_operands(core) {
+        let es = match self.words_to_operands() {
             Ok(data)     => data, 
             Err(err_msg) => {
                 eprintln!("sush: {}", err_msg);
@@ -56,50 +47,6 @@ impl Calc {
                 eprintln!("sush: {}: {}", &self.text, msg);
                 None
             },
-        }
-    }
-
-    fn word_to_i64(name: &str, core: &mut ShellCore, inc: i64, pre: bool) -> Result<i64, String> {
-        if ! is_name(name, core) {
-            return match inc != 0 && ! pre {
-                true  => Err(syntax_error_msg(name)),
-                false => Self::str_to_num(&name, core),
-            }
-        }
-
-        let num_i64 = match Self::str_to_num(&name, core) {
-            Ok(n)        => n,
-            Err(err_msg) => return Err(err_msg), 
-        };
-        
-        core.data.set_param(name, &(num_i64 + inc).to_string());
-
-        match pre {
-            true  => Ok(num_i64+inc),
-            false => Ok(num_i64),
-        }
-    }
-
-    fn word_to_operand(w: &Word, pre_increment: i64, post_increment: i64,
-                       core: &mut ShellCore) -> Result<CalcElement, String> {
-        if pre_increment != 0 && post_increment != 0 
-        || w.text.find('\'').is_some() {
-            return Err(syntax_error_msg(&w.text));
-        }
-
-        let name = match w.eval_as_value(core) {
-            Some(v) => v, 
-            None => return Err(format!("{}: wrong substitution", &w.text)),
-        };
-
-        let res = match pre_increment {
-            0 => Self::word_to_i64(&name, core, post_increment, false),
-            _ => Self::word_to_i64(&name, core, pre_increment, true),
-        };
-
-        match res {
-            Ok(n)  => return Ok(CalcElement::Operand(n)),
-            Err(e) => return Err(e),
         }
     }
 
@@ -120,7 +67,7 @@ impl Calc {
         0
     }
 
-    fn words_to_operands(&mut self, core: &mut ShellCore) -> Result<Vec<CalcElement>, String> {
+    fn words_to_operands(&mut self) -> Result<Vec<CalcElement>, String> {
         let mut ans = vec![];
         let mut pre_increment = 0;
 
@@ -128,7 +75,6 @@ impl Calc {
         for i in 0..len {
             let e = self.elements[i].clone();
             pre_increment = match e {
-                //CalcElement::Word(w, post_increment) => {
                 CalcElement::Word(_, _) => {
                     match pre_increment {
                         1  => ans.push(CalcElement::PlusPlus),
@@ -137,17 +83,6 @@ impl Calc {
                     }
 
                     ans.push(e);
-
-                    /*
-                    if let Some(CalcElement::Operand(_)) = ans.last() {
-                        return Err(syntax_error_msg(&w.text));
-                    }
-
-                    match Self::word_to_operand(&w, pre_increment, post_increment, core) {
-                        Ok(n)    => ans.push(n),
-                        Err(msg) => return Err(msg),
-                    }
-                    */
                     0
                 },
                 CalcElement::Increment(n) => self.inc_dec_to_unarys(&mut ans, i, n),
@@ -162,31 +97,6 @@ impl Calc {
             1  => Err(syntax_error_msg("++")),
             -1 => Err(syntax_error_msg("--")),
             _  => Ok(ans),
-        }
-    }
-
-    fn str_to_num(name: &str, core: &mut ShellCore) -> Result<i64, String> {
-        let mut name = name.to_string();
-
-        const RESOLVE_LIMIT: i32 = 10000;
-
-        for i in 0..RESOLVE_LIMIT {
-            match is_name(&name, core) {
-                true  => name = core.data.get_param(&name),
-                false => break,
-            }
-
-            if i == RESOLVE_LIMIT - 1 {
-                return Err(recursion_error(&name));
-            }
-        }
-
-        if let Ok(n) = name.parse::<i64>() {
-            Ok( n )
-        }else if name == "" || is_name(&name, core) {
-            Ok( 0 )
-        }else{
-            Err(syntax_error_msg(&name))
         }
     }
 
