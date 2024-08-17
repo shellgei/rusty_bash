3a73baa9cc1e1d44c135e8d1adca6867ad442bf3
Ryuichi Ueda <ryuichiueda@gmail.com>
2024-08-17 18:48:10 +0900

    Rewind

--- src/elements/word/brace_expansion.rs
+++ src/elements/word/brace_expansion.rs
@@ -5,7 +5,6 @@ use crate::elements::subword::Subword;
 use super::Word;
 
 pub fn eval(word: &mut Word) -> Vec<Word> {
-    invalidate_brace(&mut word.subwords);
     for i in open_brace_pos(word) {
         let d = parse(&word.subwords[i..], i);
         if d.len() > 2 {
@@ -64,13 +63,12 @@ fn get_delimiters(stack: &mut Vec<&str>, start: usize) -> Vec<usize> {
 
 fn expand(subwords: &Vec<Box<dyn Subword>>, delimiters: &Vec<usize>) -> Vec<Word> {
     let left = &subwords[..delimiters[0]];
-    let mut right = subwords[(delimiters.last().unwrap()+1)..].to_vec();
-    invalidate_brace(&mut right);
+    let right = &subwords[(delimiters.last().unwrap()+1)..];
 
     let mut ans = vec![];
     for i in 0..(delimiters.len()-1) {
         let center = &subwords[ (delimiters[i]+1)..delimiters[i+1] ];
-        let mut w = Word::new([ left, &center, &right ].concat() );
+        let mut w = Word::new([ left, &center, right ].concat() );
         ans.append(&mut eval(&mut w));
     }
     ans
