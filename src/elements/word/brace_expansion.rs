ba8cbf972767add72a7d966d9b5fcf89996eae94
Ryuichi Ueda <ryuichiueda@gmail.com>
2024-08-17 17:13:00 +0900

    Fix

--- src/elements/word/brace_expansion.rs
+++ src/elements/word/brace_expansion.rs
@@ -5,6 +5,7 @@ use crate::elements::subword::Subword;
 use super::Word;
 
 pub fn eval(word: &mut Word) -> Vec<Word> {
+    invalidate_brace(&mut word.subwords);
     for i in open_brace_pos(word) {
         let d = parse(&word.subwords[i..], i);
         if d.len() > 2 {
