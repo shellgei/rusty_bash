cbecc66e16373a47e6a1f36197e0da6215c5b149
Ryuichi Ueda <ryuichiueda@gmail.com>
2024-09-08 09:39:03 +0900

    Clean

--- src/elements/subword/arithmetic.rs
+++ src/elements/subword/arithmetic.rs
@@ -8,7 +8,6 @@ use crate::elements::subword::Subword;
 #[derive(Debug, Clone)]
 pub struct Arithmetic {
     pub text: String,
-    //pub arith: Vec<ArithmeticExpr>,
     com: ArithmeticCommand,
 }
 
@@ -26,63 +25,18 @@ impl Subword for Arithmetic {
 }
 
 impl Arithmetic {
-    /*
-    fn new() -> Self {
-        Self {
-            text: String::new(),
-            arith: vec![],
-        }
-    }*/
-
     pub fn parse(feeder: &mut Feeder, core: &mut ShellCore) -> Option<Self> {
         if ! feeder.starts_with("$((") {
             return None;
         }
         feeder.set_backup();
-        feeder.consume(1);
+        let dl = feeder.consume(1);
 
-        match ArithmeticCommand::parse(feeder, core) {
-            Some(a) => {
-                let ans = Arithmetic{ text: "$".to_owned() + &a.text.clone(), com: a};
-                //Some(Arithmetic{ text: "$".to_owned() + &a.text.clone(), com: a})
-                    feeder.pop_backup();
-                Some(ans)
-            },
-            None => {
-                feeder.rewind();
-                None
-            },
+        if let Some(a) = ArithmeticCommand::parse(feeder, core) {
+            feeder.pop_backup();
+            return Some(Arithmetic{ text: dl + &a.text.clone(), com: a});
         }
-        /*
-        feeder.set_backup();
-
-        let mut ans = Self::new();
-        ans.text = feeder.consume(3);
-
-        loop {
-            if let Some(c) = ArithmeticExpr::parse(feeder, core) {
-                if feeder.starts_with(",") {
-                    ans.text += &c.text;
-                    ans.text += &feeder.consume(1);
-                    ans.arith.push(c);
-                    continue;
-                }
-
-                if feeder.starts_with("))") {
-                    ans.text += &c.text;
-                    ans.text += &feeder.consume(2);
-                    ans.arith.push(c);
-                    feeder.pop_backup();
-                    return Some(ans);
-                }
-    
-                break;
-            }else{
-                break;
-            }
-        }
-
         feeder.rewind();
-        */
+        None
     }
 }
