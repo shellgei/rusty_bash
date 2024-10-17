55c453f38df4660527d8096b391fd2bcf507d3c7
Ryuichi Ueda <ryuichiueda@gmail.com>
2024-10-17 10:50:06 +0900

    Add a function

--- src/elements/word/tilde_expansion.rs
+++ src/elements/word/tilde_expansion.rs
@@ -4,4 +4,27 @@
 use crate::ShellCore;
 use crate::elements::word::Word;
 
-pub fn eval(_: &mut Word, _: &mut ShellCore) { }
+pub fn eval(word: &mut Word, core: &mut ShellCore) {
+    let length = match prefix_length(word) {
+        0 => return,
+        n => n,
+    };
+
+    let text: String = word.subwords[1..length].iter()
+               .map(|e| e.get_text().to_string())
+               .collect::<Vec<String>>()
+               .concat();
+
+    eprintln!("PREFIX: {}", text);
+}
+
+fn prefix_length(word: &Word) -> usize {
+    if word.subwords.len() == 0 || word.subwords[0].get_text() != "~" {
+        return 0;
+    }
+
+    match word.subwords.iter().position(|e| e.get_text() == "/") {
+        None    => word.subwords.len(),
+        Some(n) => n,
+    }
+}
