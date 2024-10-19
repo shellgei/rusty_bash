0d9c82f5db2ea84d3bf1000dde8d396cc14b9867
Ryuichi Ueda <ryuichiueda@gmail.com>
2024-10-19 13:46:07 +0900

    Remove a test case for Mac

--- test/test_others.bash
+++ test/test_others.bash
@@ -280,8 +280,8 @@ b=$(ls aaaaaaaaaaaaaa 2>&1 | wc -l)
 res=$($com <<< 'ls aaaaaaaaaaaaaa 2>&1 | wc -l')
 [ "$b" == "$res" ] || err $LINENO
 
-res=$($com <<< 'pwd 200>&100')
-[ "$?" == "1" ] || err $LINENO
+#res=$($com <<< 'pwd 200>&100')
+#[ "$?" == "1" ] || err $LINENO
 
 res=$($com <<< 'ls 200>&100')
 [ "$?" == "1" ] || err $LINENO
