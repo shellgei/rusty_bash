f34e1f3fe27872c6c261c4b2daae1cca619448d3
Ryuichi Ueda <ryuichiueda@gmail.com>
2024-11-08 15:13:51 +0900

    Simplify

--- src/elements/word/split.rs
+++ src/elements/word/split.rs
@@ -3,35 +3,32 @@
 
 use crate::ShellCore;
 use crate::elements::word::Word;
-use crate::elements::subword::Subword;
 
 pub fn eval(word: &Word, core: &mut ShellCore) -> Vec<Word> {
-    let (pos, mut split) = find_pos(word);
-    if split.len() == 0 {
+    let (pos, mut words) = split(word);
+    if words.len() == 0 {
         return vec![word.clone()];
     }
 
-    let gen_word = |sws| Word{ text: String::new(), subwords: sws};
+    words[0].subwords = vec![
+                            word.subwords[..pos].to_vec(),
+                            words[0].subwords.clone()
+                        ].concat();
 
-    let mut left = gen_word(word.subwords[..pos].to_vec());
-    left.subwords.push(split.remove(0));
-
-    let mut ans = vec![left];
-    while split.len() >= 2 {
-        ans.push(gen_word(vec![split.remove(0)]));
-    }
-
-    let mut right = gen_word(word.subwords[pos+1..].to_vec());
-    right.subwords.insert(0, split.remove(0));
-
-    [ ans, eval(&right, core) ].concat()
+    let mut right = words.pop().unwrap();
+    right.subwords.append(&mut word.subwords[pos+1..].to_vec());
+    
+    [ words, eval(&right, core) ].concat()
 }
 
-pub fn find_pos(word: &Word) -> (usize, Vec<Box<dyn Subword>>) {
+pub fn split(word: &Word) -> (usize, Vec<Word>) {
     for (i, sw) in word.subwords.iter().enumerate() {
-        let split = sw.split();
-        if split.len() >= 2 {
-            return (i, split);
+        let subwords = sw.split();
+        if subwords.len() >= 2 {
+            let words = subwords.iter()
+                        .map(|s| Word::new(vec![s.clone()]))
+                        .collect();
+            return (i, words);
         }
     }
     (0, vec![])
