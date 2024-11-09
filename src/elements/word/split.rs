6b277679824cdbf180859fa05972887ea46b6c3c
Ryuichi Ueda <ryuichiueda@gmail.com>
2024-11-09 16:13:09 +0900

    Simplify

--- src/elements/word/split.rs
+++ src/elements/word/split.rs
@@ -6,25 +6,24 @@ use crate::elements::word::Word;
 use crate::elements::subword::Subword;
 
 pub fn eval(word: &Word, core: &mut ShellCore) -> Vec<Word> {
-    let (pos, mut subws) = split(word);
-    if subws.len() == 0 {
-        return vec![word.clone()];
-    }
-    let (lsubws, rsubws) = (&word.subwords[..pos], &word.subwords[pos+1..]);
+    let (left_sws, mut center_sws, right_sws) = match split(word) {
+        (Some(i), sws) => (&word.subwords[..i], sws, &word.subwords[i+1..]),
+        (None, _)      => return vec![word.clone()],
+    };
 
-    let left = Word::from([ lsubws, &[subws.remove(0)] ].concat());
-    let right = Word::from([ &[subws.pop().unwrap()], rsubws].concat());
-    let centers = subws.iter().map(|s| Word::from(s.clone())).collect();
+    let left = Word::from([ left_sws, &[center_sws.remove(0)] ].concat());
+    let right = Word::from([ &[center_sws.pop().unwrap()], right_sws].concat());
+    let centers = center_sws.iter().map(|s| Word::from(s.clone())).collect();
 
     [ vec![left], centers, eval(&right, core) ].concat()
 }
 
-pub fn split(word: &Word) -> (usize, Vec<Box::<dyn Subword>>) {
+pub fn split(word: &Word) -> (Option<usize>, Vec<Box::<dyn Subword>>) {
     for (i, sw) in word.subwords.iter().enumerate() {
         let subwords = sw.split();
         if subwords.len() >= 2 {
-            return (i, subwords);
+            return (Some(i), subwords);
         }
     }
-    (0, vec![])
+    (None, vec![])
 }
