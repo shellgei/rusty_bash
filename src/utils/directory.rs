90b8d54d9645e1cafce1bba400696f910ea13dcc
Ryuichi Ueda <ryuichiueda@gmail.com>
2024-11-17 20:14:39 +0900

    Simplify

--- src/utils/directory.rs
+++ src/utils/directory.rs
@@ -1,7 +1,7 @@
 //SPDX-FileCopyrightText: 2024 Ryuichi Ueda <ryuichiueda@gmail.com>
 //SPDX-License-Identifier: BSD-3-Clause
 
-use std::fs::DirEntry;
+use std::ffi::OsString;
 use std::path::Path;
 
 pub fn files(dir: &str) -> Vec<String> {
@@ -10,10 +10,12 @@ pub fn files(dir: &str) -> Vec<String> {
         d  => Path::new(d).read_dir(),
     };
 
-    let to_str = |p: DirEntry| p.file_name().to_string_lossy().to_string();
-
-    match readdir {
-        Ok(rd) => rd.map(|e| to_str(e.unwrap()) ).collect(),
-        _      => vec![],
+    if ! readdir.is_ok() {
+        return vec![];
     }
+
+    let f = |e: OsString| e.to_string_lossy().to_string();
+    readdir.unwrap()
+        .map(|e| f(e.unwrap().file_name()) )
+        .collect()
 }
