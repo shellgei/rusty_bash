6d818e688f76a1b036929ad2a6979e06956e21b1
Ryuichi Ueda <ryuichiueda@gmail.com>
2024-11-17 14:38:15 +0900

    Implementing

--- src/elements/word/path_expansion.rs
+++ src/elements/word/path_expansion.rs
@@ -1,14 +1,25 @@
 //SPDX-FileCopyrightText: 2024 Ryuichi Ueda ryuichiueda@gmail.com
 //SPDX-License-Identifier: BSD-3-Clause
 
+use crate::elements::subword::Subword;
 use crate::elements::word::Word;
+use super::subword::simple::SimpleSubword;
 
 pub fn eval(word: &mut Word) -> Vec<Word> {
     let paths = expand(&word.make_glob_string());
-    vec![word.clone()]
+    if paths.len() == 0 {
+        return vec![word.clone()];
+    }
+
+    let subwd = |path| Box::new(SimpleSubword{ text: path });
+    let wd = |path| Word::from( subwd(path) as Box::<dyn Subword>);
+    paths.iter().map(|p| wd(p.to_string())).collect()
 }
 
 fn expand(globstr: &str) -> Vec<String> {
-    eprintln!("glob: {:?}", &globstr);
-    vec![]
+    if "*?@+![".chars().all(|c| ! globstr.contains(c)) {
+        return vec![];
+    }
+
+    directory::glob("", &globstr, extglob)
 }
