da93d7bb64a15822fc8eba99b97b1d17a56d66ce
Ryuichi Ueda <ryuichiueda@gmail.com>
2024-12-06 16:52:22 +0900

    Fix

--- src/utils/glob/comparator.rs
+++ src/utils/glob/comparator.rs
@@ -4,5 +4,40 @@
 use super::GlobElem;
 
 pub fn shave_word(word: &String, pattern: &Vec<GlobElem>) -> Vec<String> {
-    vec!["".to_string()]
+    let mut candidates = vec![word.to_string()];
+    pattern.iter().for_each(|w| shave(&mut candidates, &w) );
+    candidates
+}
+
+pub fn shave(candidates: &mut Vec<String>, w: &GlobElem) {
+    match w {
+        GlobElem::Normal(s) => nonspecial(candidates, &s),
+        GlobElem::Symbol('?') => question(candidates),
+        _ => panic!("Unknown glob symbol"),
+    }
+}
+
+fn nonspecial(cands: &mut Vec<String>, s: &String) {
+    let mut ans = vec![];
+
+    for c in cands.into_iter() {
+        if ! c.starts_with(s) {
+            continue;
+        }
+
+        ans.push(c[s.len()..].to_string());
+    }
+
+    *cands = ans;
+}
+
+fn question(cands: &mut Vec<String>) {
+    let mut ans = vec![];
+    for cand in cands.into_iter() {
+        if let Some(c) = cand.chars().nth(0) {
+            let len = c.len_utf8();
+            ans.push(cand[len..].to_string());
+        }
+     }
+    *cands = ans;
 }
