1a14ee1601a7babe5b9699cfdbcc7157336c783b
Ryuichi Ueda <ryuichiueda@gmail.com>
2024-12-06 13:44:07 +0900

    Implement

--- src/elements/word/path_expansion.rs
+++ src/elements/word/path_expansion.rs
@@ -2,17 +2,25 @@
 //SPDX-License-Identifier: BSD-3-Clause
 
 use crate::elements::word::Word;
-use crate::utils::glob;
+use crate::utils::directory;
 
 pub fn eval(word: &mut Word) -> Vec<Word> {
     let paths = expand(&word.make_glob_string());
-    vec![word.clone()]
+    if paths.len() == 0 {
+        return vec![word.clone()];
+    }
+
+    paths.iter().map(|p| Word::from(p.as_str())).collect()
 }
 
 fn expand(pattern: &str) -> Vec<String> {
-    if "*?@+![".chars().all(|c| ! pattern.contains(c)) {
+    if "*?+![".chars().all(|c| ! pattern.contains(c)) {
         return vec![];
     }
 
-    directory::glob("", patttern)
+    let div = pattern.split("/");
+    let last = div.last().unwrap();
+    let dir = &pattern[0..pattern.len()-last.len()];
+    directory::glob(dir, pattern)
 }
+
--- src/utils/directory.rs
+++ src/utils/directory.rs
@@ -1,38 +1,21 @@
-//SPDX-FileCopyrightText: 2024 Ryuichi Ueda <ryuichiueda@gmail.com>
-//SPDX-License-Identifier: BSD-3-Clause
-
-use crate::utils::file;
+use std::fs::DirEntry;
 use std::path::Path;
-use super::glob;
 
 pub fn files(dir: &str) -> Vec<String> {
-    let readdir = match dir {
-        "" => Path::new(".").read_dir(),
-        d  => Path::new(d).read_dir(),
+    let dir = if dir == "" {"."}else{dir};
+
+    let entries = match Path::new(dir).read_dir() {
+        Ok(es) => es,
+        Err(_) => return vec![],
     };
 
-    if ! readdir.is_ok() {
-        return vec![];
-    }
+    let f = |e: DirEntry| e.file_name()
+               .to_string_lossy().to_string();
 
-    readdir.unwrap()
-        .map(|e| file::oss_to_name(&e.unwrap().file_name()) )
-        .collect()
+    entries.map(|e| f(e.unwrap()) ).collect()
 }
 
-pub fn glob(dir: &str, pattern: &str, extglob: bool) -> Vec<String> {
-    let make_path = |file| dir.to_owned() + file + "/";
-
-    if ["", ".", ".."].contains(&pattern) {
-        return vec![make_path(pattern)];
-    }
-
-    let mut fs = files(dir);
-    fs.append( &mut vec![".".to_string(), "..".to_string()] );
-
-    let pat = glob::parse(pattern, extglob);
-    let compare = |file: &String| ( ! file.starts_with(".") || pattern.starts_with(".") )
-                            && glob::compare(file, &pat);
-
-    fs.iter().filter(|f| compare(f) ).map(|f| make_path(f) ).collect()
+pub fn glob(dir: &str, pattern: &str) -> Vec<String> {
+    files(dir)
 }
+
