97eca6e196c1fd30767cfbf02c4103f17bf117e5
Ryuichi Ueda <ryuichiueda@gmail.com>
2025-01-02 16:10:43 +0900

    Fix while and for about blanks

--- src/elements/command/while.rs
+++ src/elements/command/while.rs
@@ -55,8 +55,13 @@ impl Command for WhileCommand {
 impl WhileCommand {
     pub fn parse(feeder: &mut Feeder, core: &mut ShellCore) -> Option<WhileCommand> {
         let mut ans = Self::default();
-        if command::eat_inner_script(feeder, core, "while", vec!["do"], &mut ans.while_script, false)
-        && command::eat_inner_script(feeder, core, "do", vec!["done"],  &mut ans.do_script, false) {
+        if ! command::eat_inner_script(feeder, core, "while", vec!["do"],
+                                       &mut ans.while_script, false){
+            return None;
+        }
+        while command::eat_blank_with_comment(feeder, core, &mut ans.text) {}
+
+        if command::eat_inner_script(feeder, core, "do", vec!["done"],  &mut ans.do_script, false) {
             ans.text.push_str("while");
             ans.text.push_str(&ans.while_script.as_mut().unwrap().get_text());
             ans.text.push_str("do");
