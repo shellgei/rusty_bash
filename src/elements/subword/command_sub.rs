d98f3b79187fd205e322826266810d08749f8a71
Ryuichi Ueda <ryuichiueda@gmail.com>
2025-02-18 17:46:13 +0900

    Add required functions

--- src/elements/subword/command_sub.rs
+++ src/elements/subword/command_sub.rs
@@ -2,11 +2,16 @@
 //SPDX-License-Identifier: BSD-3-Clause
 
 use crate::{ShellCore, Feeder};
+use crate::elements::Pipe;
 use crate::elements::command::Command;
 use crate::elements::command::paren::ParenCommand;
 use crate::elements::subword::Subword;
 use crate::error::parse::ParseError;
 use crate::error::exec::ExecError;
+use nix::unistd;
+use std::fs::File;
+use std::io::{BufReader, BufRead, Error};
+use std::os::fd::{FromRawFd, RawFd};
 
 #[derive(Debug, Clone)]
 pub struct CommandSubstitution {
@@ -18,13 +23,42 @@ impl Subword for CommandSubstitution {
     fn get_text(&self) -> &str {&self.text.as_ref()}
     fn boxed_clone(&self) -> Box<dyn Subword> {Box::new(self.clone())}
 
-    fn substitute(&mut self, _: &mut ShellCore) -> Result<(), ExecError> {
-        dbg!("{:?}", &self.text);
-        Ok(())
+    fn substitute(&mut self, core: &mut ShellCore) -> Result<(), ExecError> {
+        let mut pipe = Pipe::new("|".to_string());
+        pipe.set(-1, unistd::getpgrp());
+        let pid = self.command.exec(core, &mut pipe)?;
+        let result = self.read(pipe.recv, core);
+        core.wait_pipeline(vec![pid]);
+        result
     }
 }
 
 impl CommandSubstitution {
+    fn set_line(&mut self, line: Result<String, Error>) -> bool {
+        if let Ok(ln) = line {
+            self.text.push_str(&ln);
+            self.text.push('\n');
+            return true;
+        }
+        false
+    }
+
+    fn read(&mut self, fd: RawFd, core: &mut ShellCore) -> Result<(), ExecError> {
+        let f = unsafe { File::from_raw_fd(fd) };
+        let reader = BufReader::new(f);
+        self.text.clear();
+        for (i, line) in reader.lines().enumerate() {
+            if ! self.set_line(line) {
+                break;
+            }
+        }
+
+        if ! self.text.is_empty() {
+            self.text.pop();
+        }
+        Ok(())
+    }
+
     pub fn parse(feeder: &mut Feeder, core: &mut ShellCore)
                          -> Result<Option<Self>, ParseError> {
         if ! feeder.starts_with("$(") {
