b7c0d6432019b22b636c7a11469a2d692dd0d79b
Ryuichi Ueda <ryuichiueda@gmail.com>
2025-02-19 17:14:48 +0900

    Implement type -p

--- src/core/builtins/type_.rs
+++ src/core/builtins/type_.rs
@@ -5,7 +5,7 @@
 use crate::{file_check, ShellCore};
 use crate::utils::{arg, file};
 
-fn print_command_type(core: &mut ShellCore, com: &String) -> i32 {
+fn type_no_opt(core: &mut ShellCore, com: &String) -> i32 {
     if core.aliases.contains_key(com) {
         println!("{} is aliased to `{}'", &com, &core.aliases[com]);
         return 0;
@@ -19,7 +19,7 @@ fn print_command_type(core: &mut ShellCore, com: &String) -> i32 {
         println!("{} is a shell builtin", com);
         return 0;
     }
-    if let Some(path) = file::search_command(com) {//TODO: show in the fullpath case
+    if let Some(path) = file::search_command(com) {
         println!("{} is {}", com, &path);
         return 0;
     }
@@ -30,19 +30,46 @@ fn print_command_type(core: &mut ShellCore, com: &String) -> i32 {
     1
 }
 
+fn type_p(core: &mut ShellCore, com: &String) -> i32 {
+    if core.aliases.contains_key(com) 
+    || core.db.functions.contains_key(com)
+    || core.builtins.contains_key(com) {
+        return 0;
+    }
+
+    if let Some(path) = file::search_command(com) {
+        println!("{}", &path);
+        return 0;
+    }
+    if file_check::is_executable(com) {
+        println!("{}", com);
+        return 0;
+    }
+    1
+}
+
 pub fn type_(core: &mut ShellCore, args: &mut Vec<String>) -> i32 {
     if args.len() < 2 {
         return 0;
     }
 
+    let mut exit_status = 0;
     let mut args = arg::dissolve_options(args);
+    if arg::consume_option("-p", &mut args) {
+        for a in &args[1..] {
+             exit_status += type_p(core, a);
+        }
+        if exit_status > 1 {
+            exit_status = 1;
+        }
+        return exit_status;
+    }
     if arg::consume_option("-P", &mut args) {
         return 0;
     }
 
-    let mut exit_status = 0;
     for a in &args[1..] {
-         exit_status += print_command_type(core, a);
+         exit_status += type_no_opt(core, a);
     }
 
     if exit_status > 1 {
