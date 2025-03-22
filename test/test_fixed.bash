09ce6b7b1700ebf2618eefb5668206ba5ac76f7d
Ryuichi Ueda <ryuichiueda@gmail.com>
2025-03-22 09:31:57 +0900

    Add a test case

--- test/test_fixed.bash
+++ test/test_fixed.bash
@@ -5,6 +5,7 @@
 err () {
 	echo $0 >> ./error
 	echo "ERROR!" FILE: $0, LINENO: $1
+	rm -f $tmp-*
 	exit 1
 }
 
@@ -13,6 +14,7 @@ err () {
 
 cd $(dirname $0)
 com=../target/release/sush
+tmp=/tmp/$$
 
 res=$($com <<< 'echo "aaa\bb" | ( read -r a ; echo $a )' )
 [ "$res" = "aaa\bb" ] || err $LINENO
@@ -49,6 +51,34 @@ AAA
 A _ ()
 t _ fofo *(f*(o))" ] || err $LINENO
 
+rm -f $tmp-*
 echo $0 >> ./ok
 exit
 
+### issue 130 ###
+### input-line.sh test of Bash ###
+
+# It works.
+cat << 'EOF' > $tmp-script
+read a
+echo @$a
+EOF
+chmod +x $tmp-script
+res=$(bash << EOF
+$com $tmp-script
+OH
+EOF
+)
+[ "$res" = "@OH" ] || err $LINENO
+
+# It doesn't work.
+# Maybe the exec-on-close is applied to
+# the file discriptor of $com << EOF. 
+
+chmod +x $tmp-script
+res=$($com << EOF
+$com $tmp-script
+OH
+EOF
+)
+[ "$res" = "@OH" ] || err $LINENO
