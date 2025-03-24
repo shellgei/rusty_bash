183b52fc3be86e881a4bd2cf7d645d7d5315b5fb
Ryuichi Ueda <ryuichiueda@gmail.com>
2025-03-24 17:30:05 +0900

    Simplify

--- src/elements/subword/double_quoted.rs
+++ src/elements/subword/double_quoted.rs
@@ -33,7 +33,6 @@ impl DoubleQuoted {
                 let txt = feeder.consume(ch.len_utf8());
                 ans.text += &txt;
                 ans.subwords.push( Box::new(SimpleSubword{ text: txt }) );
-                return Ok(true);
             },
             None     => feeder.feed_additional_line(core)?,
         }
