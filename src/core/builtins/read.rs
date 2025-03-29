ea801333be02328131caaf22a7331106f8c0a4a3
Ryuichi Ueda <ryuichiueda@gmail.com>
2025-03-29 14:34:18 +0900

    Clean

--- src/core/builtins/read.rs
+++ src/core/builtins/read.rs
@@ -1,11 +1,8 @@
 //SPDX-FileCopyrightText: 2024 Ryuichi Ueda <ryuichiueda@gmail.com>
 //SPDX-License-Identifier: BSD-3-Clause
 
-use crate::{Feeder, ShellCore};
+use crate::ShellCore;
 use crate::{arg, error};
-use crate::builtins::error_exit;
-use crate::elements::command;
-use crate::elements::word::{Word, WordMode};
 
 fn is_varname(s :&String) -> bool {
     if s.is_empty() {
@@ -21,21 +18,6 @@ fn is_varname(s :&String) -> bool {
     let name_c = |c| ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z')
                      || ('0' <= c && c <= '9') || '_' == c;
     s.chars().position(|c| !name_c(c)) == None
-}
-
-fn remove_escape(text: &str) -> String {
-    let mut escape = false;
-    let mut ans = String::new();
-    for ch in text.chars() {
-        if ch == '\\' {
-            escape = !escape;
-            if escape {
-                continue;
-            }
-        }
-        ans.push(ch);
-    }
-    ans
 }
 
 pub fn read_(core: &mut ShellCore, args: &mut Vec<String>, ignore_escape: bool) -> i32 {
@@ -57,7 +39,6 @@ pub fn read_(core: &mut ShellCore, args: &mut Vec<String>, ignore_escape: bool) 
 
     args.remove(0);
     while args.len() > 0 && ! remaining.is_empty() {
-
         let mut word = match eat_word(core, &mut remaining, &ifs, ignore_escape) {
             Some(w) => w,
             None => break,
@@ -87,66 +68,42 @@ pub fn read_(core: &mut ShellCore, args: &mut Vec<String>, ignore_escape: bool) 
     0
 }
 
-pub fn read_a(core: &mut ShellCore, name: &String) -> i32 {
-    let mut line = String::new();
+pub fn read_a(core: &mut ShellCore, name: &String, ignore_escape: bool) -> i32 {
+    let mut remaining = String::new();
     let len = std::io::stdin()
-        .read_line(&mut line)
+        .read_line(&mut remaining)
         .expect("SUSHI INTERNAL ERROR: Failed to read line");
-    let mut feeder = Feeder::new(&line);
-    let mut tmp = String::new();
+
+    if len == 0 {
+        return 1;
+    }
+
+    let ifs = match core.db.has_value("IFS") {
+        true  => core.db.get_param("IFS").unwrap(),
+        false => " \t\n".to_string(),
+    };
+
+    consume_ifs(&mut remaining, " \t");
 
     let mut pos = 0;
-    loop {
-        command::eat_blank_with_comment(&mut feeder, core, &mut tmp);
-        if let Ok(Some(w)) = Word::parse(&mut feeder, core, Some(WordMode::ReadCommand)) {
-            let text = remove_escape(&w.text);
-            if let Err(_) = core.db.set_array_elem(name, &text, pos, None) {
-                return error_exit(1, "read", "array allocation error", core);
-            }
-            pos +=1;
-        }else{
-            break;
-        }
-    }
+    while ! remaining.is_empty() {
+        let mut word = match eat_word(core, &mut remaining, &ifs, ignore_escape) {
+            Some(w) => w,
+            None => break,
+        };
+        consume_tail_ifs(&mut word, " \t\n");
 
-    match len == 0 {
-        true  => 1,
-        false => 0,
-    }
-}
-
-/*
-pub fn read_r(core: &mut ShellCore, args: &mut Vec<String>) -> i32 {
-    let mut line = String::new();
-    let len = std::io::stdin()
-        .read_line(&mut line)
-        .expect("SUSHI INTERNAL ERROR: Failed to read line");
-
-    let mut pos = 1;
-    let mut surplus = vec![];
-    for w in line.trim_end().split(' ') {
-        if pos < args.len()-1 {
-            if ! set_to_param(core, args, pos, &w) {
-                return 1;
-            }
-            pos +=1;
-        }else{
-            surplus.push(w);
-        }
-    }
-
-    if ! surplus.is_empty() {
-        if ! set_to_param(core, args, args.len()-1, &surplus.join(" ")) {
+        if let Err(e) = core.db.set_array_elem(name, &word, pos, None) {
+            let msg = format!("{:?}", &e);
+            error::print(&msg, core);
             return 1;
         }
+        pos += 1;
+        consume_ifs(&mut remaining, &ifs);
     }
 
-    match len == 0 {
-        true  => 1,
-        false => 0,
-    }
+    0
 }
-*/
 
 pub fn read(core: &mut ShellCore, args: &mut Vec<String>) -> i32 {
     if args.len() <= 1 {
@@ -154,10 +111,11 @@ pub fn read(core: &mut ShellCore, args: &mut Vec<String>) -> i32 {
     }
 
     let mut args = arg::dissolve_options(args);
+    let r_opt = arg::consume_option("-r", &mut args);
+                                                      //
     if let Some(a) = arg::consume_with_next_arg("-a", &mut args) {
-        return read_a(core, &a);
+        return read_a(core, &a, r_opt);
     }
-    let r_opt = arg::consume_option("-r", &mut args); //TODO: change the precedure
 
     for a in &args[1..] {
         if ! is_varname(&a) {
@@ -172,10 +130,7 @@ pub fn read(core: &mut ShellCore, args: &mut Vec<String>) -> i32 {
         }
     }
 
-    match r_opt {
-        true  => read_(core, &mut args, true),
-        false => read_(core, &mut args, false),
-    }
+    read_(core, &mut args, r_opt)
 }
 
 pub fn eat_word(core: &mut ShellCore, remaining: &mut String, ifs: &str, ignore_escape: bool) -> Option<String> {
