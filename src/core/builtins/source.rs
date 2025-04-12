c1139ace3e26ed103f4f62475ba7ef29ee3153cf
Ryuichi Ueda <ryuichiueda@gmail.com>
2025-04-12 14:23:38 +0900

    Remove warnings

--- src/core/builtins/source.rs
+++ src/core/builtins/source.rs
@@ -2,9 +2,6 @@
 //SPDX-License-Identifier: BSD-3-Clause
 
 use crate::{file_check, Script, ShellCore, Feeder};
-use crate::elements::io;
-use std::fs::File;
-use std::os::fd::IntoRawFd;
 
 pub fn source(core: &mut ShellCore, args: &mut Vec<String>) -> i32 {
     if args.len() < 2 {
@@ -18,22 +15,6 @@ pub fn source(core: &mut ShellCore, args: &mut Vec<String>) -> i32 {
         return 1;
     }
 
-    /*
-    let file = match File::open(&args[1]) {
-        Ok(f)  => f, 
-        Err(e) => {
-            eprintln!("sush: {}: {}", &args[1], &e);
-            return 1;
-        }, 
-    };*/
-
-    /*
-    let fd = file.into_raw_fd();
-    let backup = io::backup(0);
-    io::replace(fd, 0);
-    let read_stdin_backup = core.read_stdin;
-    core.read_stdin = true;
-    */
     core.source_function_level += 1;
     core.source_files.push(args[1].to_string());
 
@@ -66,10 +47,8 @@ pub fn source(core: &mut ShellCore, args: &mut Vec<String>) -> i32 {
     source.remove(0);
     let _ = core.db.set_array("BASH_SOURCE", source, None);
     core.db.position_parameters.pop();
-    //io::replace(backup, 0);
     core.source_function_level -= 1;
     core.source_files.pop();
     core.return_flag = false;
-    //core.read_stdin = read_stdin_backup;
     core.db.exit_status
 }
