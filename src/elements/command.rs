6ed59bff38cad0d5eb8e7e1b2078a2f017cb33c7
Ryuichi Ueda <ryuichiueda@gmail.com>
2025-04-27 11:34:59 +0900

    Clean

--- src/elements/command.rs
+++ src/elements/command.rs
@@ -67,27 +67,6 @@ pub trait Command {
         }
     }
 
-    /*
-    fn fork_exec(&mut self, core: &mut ShellCore, pipe: &mut Pipe) -> Result<Option<Pid>, ExecError> {
-        match unsafe{unistd::fork()} {
-            Ok(ForkResult::Child) => {
-                core.initialize_as_subshell(Pid::from_raw(0), pipe.pgid);
-                io::connect(pipe, self.get_redirects(), core)?;
-                if let Err(e) = self.run(core, true) {
-                    e.print(core);
-                }
-                exit::normal(core)
-            },
-            Ok(ForkResult::Parent { child } ) => {
-                core.set_pgid(child, pipe.pgid);
-                pipe.parent_close();
-                Ok(Some(child))
-            },
-            Err(err) => panic!("sush(fatal): Failed to fork. {}", err),
-        }
-    }
-    */
-
     fn nofork_exec(&mut self, core: &mut ShellCore) -> Result<Option<Pid>, ExecError> {
         let mut result = Ok(None);
         for r in self.get_redirects().iter_mut() {
