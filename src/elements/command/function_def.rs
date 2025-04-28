bbb8f458fd2d5e34bd8e16a8f8ac28240ce6f4ff
Ryuichi Ueda <ryuichiueda@gmail.com>
2025-04-29 08:40:42 +0900

    Implement eat_*

--- src/elements/command/function_def.rs
+++ src/elements/command/function_def.rs
@@ -1,7 +1,7 @@
 //SPDX-FileCopyrightText: 2022 Ryuichi Ueda ryuichiueda@gmail.com
 //SPDX-License-Identifier: BSD-3-Clause
 
-use crate::{ShellCore, Feeder};
+use crate::{ShellCore, Feeder, utils};
 use crate::error::exec::ExecError;
 use crate::error::parse::ParseError;
 use super::{Command, Redirect};
