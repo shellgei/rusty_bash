f342a19b122df98939be9541591450101f9c5be9
Ryuichi Ueda <ryuichiueda@gmail.com>
2025-05-06 08:33:50 +0900

    Simplify

--- src/core/builtins/job_commands.rs
+++ src/core/builtins/job_commands.rs
@@ -2,7 +2,7 @@
 //SPDX-License-Identifier: BSD-3-Clause
 
 use crate::ShellCore;
-use crate::{error, signal, utils};
+use crate::{signal, utils};
 use crate::core::JobEntry;
 use crate::utils::arg;
 use nix::sys::signal::Signal;
@@ -28,95 +28,48 @@ fn jobid_to_jobentry(id: usize, jobs: &mut Vec<JobEntry>) -> Option<&mut JobEntr
     None
 }
 
-fn jobspec_to_jobid(s: &str, priority: &Vec<usize>, table: &Vec<JobEntry>) -> Result<usize, String> {
-    if s == "%+" {
-        return match priority.len() {
-            0 => Err("%+: no such job".to_string()), 
-            _ => Ok(priority[0]),
-        };
+pub fn bg(core: &mut ShellCore, args: &mut Vec<String>) -> i32 {
+    if core.job_table.is_empty() {
+        return 1;
     }
 
-    if s == "%-" {
-        return match priority.len() {
-            0 => Err("%-: no such job".to_string()), 
-            1 => Err("%-: no such job".to_string()), 
-            _ => Ok(priority[1]),
-        };
-    }
-
-    let word = &s[1..];
-    let mut ans = 0;
-    for job in table {
-        let jobname = job.text.split(" ").nth(0).unwrap();
-        if jobname == word {
-            if ans != 0 {
-                return Err((s.to_owned() + ": ambiguous job spec").to_string());
-            }
-            ans = job.id;
+    if args.len() == 1 {
+        let id = core.job_table_priority[0];
+        match jobid_to_jobentry(id, &mut core.job_table) {
+            Some(job) => job.send_cont(),
+            _ => return 1, 
+        }
+    }else if args.len() == 2 {
+        match jobspec_to_array_pos(core, &args[1]) {
+            Some(pos) => core.job_table[pos].send_cont(),
+            None      => return 1,
         }
     }
 
-    if ans != 0 {
-        return Ok(ans);
-    }
-
-    if s.starts_with("%") {
-        return match word.parse::<usize>() {
-            Ok(n)  => Ok(n),
-            Err(_) => Err((s.to_owned() + ": no such job").to_string()),
-        };
-    }
-
-    Err((s.to_owned() + ": no such job").to_string())
+    0
 }
 
-pub fn bg(core: &mut ShellCore, args: &mut Vec<String>) -> i32 {
+pub fn fg(core: &mut ShellCore, args: &mut Vec<String>) -> i32 {
     let id = if args.len() == 1 {
         if core.job_table_priority.is_empty() {
             return 1;
         }
         core.job_table_priority[0]
     }else if args.len() == 2 {
-        match jobspec_to_jobid(&args[1], &core.job_table_priority, &core.job_table) {
-            Ok(n) => n,
-            Err(s) => {
-                error::print(&("bg: ".to_owned() + &s), core);
-                return 1;
-            },
+        //match jobspec_to_jobid(&args[1], &core.job_table_priority, &core.job_table) {
+        match jobspec_to_array_pos(core, &args[1]) {
+            Some(pos) => core.job_table[pos].id,
+            None => return 1,
         }
     }else{
         return 1;
     };
 
-    match jobid_to_jobentry(id, &mut core.job_table) {
-        Some(job) => job.send_cont(),
-        _ => return 1, 
-    }
-    0
-}
-
-pub fn fg(core: &mut ShellCore, args: &mut Vec<String>) -> i32 {
     let fd = match core.tty_fd.as_ref() {
         Some(fd) => fd,
         _        => return 1,
     };
 
-    let id = if args.len() == 1 {
-        if core.job_table_priority.is_empty() {
-            return 1;
-        }
-        core.job_table_priority[0]
-    }else if args.len() == 2 {
-        match jobspec_to_jobid(&args[1], &core.job_table_priority, &core.job_table) {
-            Ok(n) => n,
-            Err(s) => {
-                error::print(&s, core);
-                return 1;
-            },
-        }
-    }else{
-        return 1;
-    };
 
     let job = match jobid_to_jobentry(id, &mut core.job_table) {
         Some(job) => job,
@@ -263,7 +216,7 @@ pub fn jobs(core: &mut ShellCore, args: &mut Vec<String>) -> i32 {
 fn wait_jobspec(core: &mut ShellCore, jobspec: &str, var_name: &Option<String>)
 -> (i32, bool) {
     match jobspec_to_array_pos(core, jobspec) {
-        Some(id) => wait_a_job(core, id, var_name),
+        Some(pos) => wait_a_job(core, pos, var_name),
         None => return (1, false),
     }
 }
