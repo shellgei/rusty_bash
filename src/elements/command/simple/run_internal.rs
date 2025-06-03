05c4477cb8b5f3986808ad49469d8132bea415d3
Ryuichi Ueda <ryuichiueda@gmail.com>
2025-06-03 15:06:45 +0900

    Simplify

--- src/elements/command/simple/run_internal.rs
+++ src/elements/command/simple/run_internal.rs
@@ -4,15 +4,12 @@
 use crate::ShellCore;
 use super::SimpleCommand;
 use crate::error::exec::ExecError;
-use crate::elements::substitution::Substitution;
 
 pub fn run(com: &mut SimpleCommand, core: &mut ShellCore)
 -> Result<bool, ExecError> {
     let ans = run_function(&mut com.args, core) 
-           || run_substitution_builtin(&mut com.args,
-                &mut com.substitutions_as_args, core)?
-           || run_builtin(&mut com.args,
-                &com.substitutions_as_args, core)?;
+           || run_substitution_builtin(com, core)?
+           || run_builtin(com, core)?;
     Ok(ans)
 }
 
@@ -23,43 +20,43 @@ fn run_function(args: &mut Vec<String>, core: &mut ShellCore) -> bool {
     }
 }
 
-pub fn run_builtin(args: &mut Vec<String>, substitutions: &Vec<Substitution>,
-                   core: &mut ShellCore) -> Result<bool, ExecError> {
-    if args.is_empty() {
+pub fn run_builtin(com: &mut SimpleCommand, core: &mut ShellCore)
+-> Result<bool, ExecError> {
+    if com.args.is_empty() {
         eprintln!("ShellCore::run_builtin");
         return Ok(false);
     }
 
-    if ! core.builtins.contains_key(&args[0]) {
+    if ! core.builtins.contains_key(&com.args[0]) {
         return Ok(false);
     }
 
     let mut special_args = vec![];
-    for sub in substitutions {
-        match args[0].as_ref() {
-            "eval" | "declare" => special_args.push(sub.get_string_for_eval(core)?),
+    for sub in &com.substitutions_as_args {
+        match com.args[0].as_ref() {
+            "eval" => special_args.push(sub.get_string_for_eval(core)?),
             _ => special_args.push(sub.text.clone()),
         }
     }
 
-    let func = core.builtins[&args[0]];
-    args.append(&mut special_args);
-    core.db.exit_status = func(core, args);
+    let func = core.builtins[&com.args[0]];
+    com.args.append(&mut special_args);
+    core.db.exit_status = func(core, &mut com.args);
     Ok(true)
 }
 
-pub fn run_substitution_builtin(args: &mut Vec<String>, substitutions: &mut Vec<Substitution>,
-                                core: &mut ShellCore) -> Result<bool, ExecError> {
-    if args.is_empty() {
+pub fn run_substitution_builtin(com: &mut SimpleCommand, core: &mut ShellCore)
+-> Result<bool, ExecError> {
+    if com.args.is_empty() {
         eprintln!("ShellCore::run_builtin");
         return Ok(false);
     }
 
-    if ! core.substitution_builtins.contains_key(&args[0]) {
+    if ! core.substitution_builtins.contains_key(&com.args[0]) {
         return Ok(false);
     }
 
-    let func = core.substitution_builtins[&args[0]];
-    core.db.exit_status = func(core, args, substitutions);
+    let func = core.substitution_builtins[&com.args[0]];
+    core.db.exit_status = func(core, &mut com.args, &mut com.substitutions_as_args);
     Ok(true)
 }
