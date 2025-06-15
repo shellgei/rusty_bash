6591a77f53508baaea648ea226f74218f3e59a8a
Ryuichi Ueda <ryuichiueda@gmail.com>
2025-06-15 17:39:33 +0900

    Fix

--- src/core/builtins/unset.rs
+++ src/core/builtins/unset.rs
@@ -19,11 +19,62 @@ fn unset_function(core: &mut ShellCore, name: &str) -> i32 {
     0
 }
 
-pub fn unset(core: &mut ShellCore, args: &mut Vec<String>) -> i32 {
-    if args.len() < 2 {
-        return 0;
-    }
+pub fn unset_one(core: &mut ShellCore, args: &mut Vec<String>) -> i32 {
+    match args[1].as_ref() {
+        "-f" => {
+            if args.len() > 2 {
+                let name = args.remove(2);
+                return unset_function(core, &name);
+            }
+        },
+        "-v" => {
+            if args.len() > 2 {
+                let name = args.remove(2);
+                return unset_var(core, &name);
+            }
+        },
+        name => {
+            let name = name.to_string();
+            args.remove(1);
+            if ! name.contains("[") {
+                return unset_all(core, &name);
+            }
 
+            let mut f = Feeder::new(&(name.to_owned()));
+            if let Ok(Some(mut sub)) = Variable::parse(&mut f, core) {
+                if let Ok(Some(key)) = sub.get_index(core, false, false) {
+                    let nm = sub.name.clone();
+                    if let Err(e) = core.db.unset_array_elem(&nm, &key) {
+                        e.print(core);
+                        return 1;
+                    }
+                    return 0;
+                }
+            }
+
+            let msg = format!("{}: invalid variable", &name);
+            return super::error_exit(1, &args[0], &msg, core);
+        },
+    }
+    0
+}
+
+pub fn unset(core: &mut ShellCore, args: &mut Vec<String>) -> i32 {
+    let mut exit_status = 0;
+
+    loop {
+        if args.len() < 2 {
+            break;
+        }
+
+        if (args[1] == "-v" || args[1] == "-f") && args.len() == 2 {
+            break;
+        }
+
+        exit_status = unset_one(core, args);
+    }
+    exit_status
+    /*
     match args[1].as_ref() {
         "-f" => {
             if args.len() > 2 {
@@ -57,4 +108,5 @@ pub fn unset(core: &mut ShellCore, args: &mut Vec<String>) -> i32 {
         },
     }
     0
+    */
 }
