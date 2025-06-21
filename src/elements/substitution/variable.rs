9fa4898dc39e09cf2f375e62c46fe2693eb48de6
Ryuichi Ueda <ryuichiueda@gmail.com>
2025-06-21 10:43:38 +0900

    Implement parser

--- src/elements/substitution/variable.rs
+++ src/elements/substitution/variable.rs
@@ -1,7 +1,22 @@
 //SPDX-FileCopyrightText: 2025 Ryuichi Ueda <ryuichiueda@gmail.com>
 //SPDX-License-Identifier: BSD-3-Clause
 
+use crate::{Feeder, ShellCore};
+use crate::error::parse::ParseError;
+
 #[derive(Debug, Clone, Default)]
 pub struct Variable {
     pub text: String,
+}
+
+impl Variable {
+    pub fn parse(feeder: &mut Feeder, core: &mut ShellCore) -> Result<Option<Self>, ParseError> {
+        let len = feeder.scanner_name(core);
+        if len == 0 {
+            return Ok(None);
+        }
+        let mut ans = Self::default();
+        ans.text = feeder.consume(len);
+        Ok(Some(ans))
+    }
 }
