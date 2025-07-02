19801f3acdbbcd88cb7d54ab527de98ff2779bde
Ryuichi Ueda <ryuichiueda@gmail.com>
2025-07-02 17:09:54 +0900

    Implement parser

--- src/elements/substitution.rs
+++ src/elements/substitution.rs
@@ -4,6 +4,8 @@
 mod value;
 mod variable;
 
+use crate::{Feeder, ShellCore};
+use crate::error::parse::ParseError;
 use self::value::Value;
 use self::variable::Variable;
 
@@ -12,4 +14,43 @@ pub struct Substitution {
     text: String,
     left_hand: Variable,
     right_hand: Value,
+}
+
+impl Substitution {
+    pub fn parse(feeder: &mut Feeder, core: &mut ShellCore)
+    -> Result<Option<Self>, ParseError> {
+        let mut ans = Self::default();
+        feeder.set_backup();
+
+        match Variable::parse(feeder, core) {
+            Ok(Some(v)) => {
+                ans.text += &v.text.clone();
+                ans.left_hand = v;
+            },
+            Ok(None) => {
+                feeder.rewind();
+                return Ok(None);
+            },
+            Err(e) => {
+                feeder.rewind();
+                return Err(e);
+            },
+        }
+
+        if ! feeder.starts_with("=") {
+            feeder.rewind();
+            return Ok(None);
+        }
+
+        ans.text += &feeder.consume(1);
+        feeder.pop_backup();
+
+        if let Some(a) = Value::parse(feeder, core)? {
+            ans.text += &a.text.clone();
+            ans.right_hand = a;
+        }
+
+        dbg!("{:?}", &ans);
+        Ok(Some(ans))
+    }
 }
