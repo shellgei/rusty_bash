d1a0eb1340edc10d06993c9091c189f050541cea
Ryuichi Ueda <ryuichiueda@gmail.com>
2025-07-19 17:05:07 +0900

    Add pipe buf size

--- src/core/builtins/ulimit.rs
+++ src/core/builtins/ulimit.rs
@@ -2,6 +2,7 @@
 //SPDX-License-Identifier: BSD-3-Clause
 
 use crate::{arg, ShellCore};
+use nix::libc;
 use nix::sys::resource;
 use nix::sys::resource::Resource;
 
@@ -14,14 +15,14 @@ fn print(soft: bool) -> i32 {
         ("file size                   ","blocks, -f", Resource::RLIMIT_FSIZE),
         ("pending signals                     ","-i", Resource::RLIMIT_SIGPENDING),
         ("max locked memory           ","kbytes, -l", Resource::RLIMIT_MEMLOCK),
-        ("max memory size             ","kbytes, -m", Resource::RLIMIT_AS),
+        ("max memory size             ","kbytes, -m", Resource::RLIMIT_RSS),
         ("open files                          ","-n", Resource::RLIMIT_NOFILE),
-        //("pipe size                (512 bytes, -p)", Resource::RLIMIT_MSGQUEUE),
+        ("pipe size                ","512 bytes, -p", Resource::RLIMIT_SIGPENDING), //dummy
         ("POSIX message queues         ","bytes, -q", Resource::RLIMIT_MSGQUEUE),
         ("real-time priority                  ","-r", Resource::RLIMIT_RTPRIO),
         ("stack size                  ","kbytes, -s", Resource::RLIMIT_STACK),
         ("cpu time                   ","seconds, -t", Resource::RLIMIT_CPU),
-        //("max user processes                  (-u", Resource::RLIMIT_RTTIME),
+        ("max user processes                  ","-u", Resource::RLIMIT_NPROC),
         ("virtual memory              ","kbytes, -v", Resource::RLIMIT_AS),
         ("file locks                          ","-x", Resource::RLIMIT_LOCKS),
     ];
@@ -31,6 +32,10 @@ fn print(soft: bool) -> i32 {
 
         let mut v = if soft { soft_limit } else { hard_limit };
         let mut infty = nix::sys::resource::RLIM_INFINITY;
+
+        if item.starts_with("pipe size") {
+            v = (libc::PIPE_BUF as u64)/512;
+        }
 
         if unit.starts_with("kbytes") {
             v /= 1024;
