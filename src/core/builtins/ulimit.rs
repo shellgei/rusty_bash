8af341e91e6d7e21e24869e9a9a9c6e902d5ce45
Ryuichi Ueda <ryuichiueda@gmail.com>
2025-07-19 16:05:54 +0900

    Add ulimit -a item

--- src/core/builtins/ulimit.rs
+++ src/core/builtins/ulimit.rs
@@ -2,16 +2,33 @@
 //SPDX-License-Identifier: BSD-3-Clause
 
 use crate::{arg, ShellCore};
+use nix::sys::resource;
+use nix::sys::resource::Resource;
 
-fn print(_: &mut ShellCore) -> i32 {
+fn print(soft: bool) -> i32 {
+    let items = [
+        (Resource::RLIMIT_RTTIME, "real-time non-blocking time  (microseconds, -R)")
+    ];
+
+    for (key, item) in items {
+        let (soft_limit, hard_limit) = resource::getrlimit(key).unwrap();
+
+        let v = if soft { soft_limit } else { hard_limit };
+        let s = match v {
+            nix::sys::resource::RLIM_INFINITY => "unlimited",
+            _ => &soft_limit.to_string(),
+        };
+    
+        println!("{} {}", &item, &s);
+    }
     0
 }
 
-pub fn ulimit(core: &mut ShellCore, args: &mut Vec<String>) -> i32 {
+pub fn ulimit(_: &mut ShellCore, args: &mut Vec<String>) -> i32 {
     let _ = arg::dissolve_options(args);
 
     if args.iter().any(|a| a == "-a"){
-        return print(core);
+        return print(true);
     }
 
     0
