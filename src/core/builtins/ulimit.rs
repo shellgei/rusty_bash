3b4757069b8d42c9c3dcee6999b5ba2425224611
Ryuichi Ueda <ryuichiueda@gmail.com>
2025-07-19 16:43:27 +0900

    Fix

--- src/core/builtins/ulimit.rs
+++ src/core/builtins/ulimit.rs
@@ -7,41 +7,39 @@ use nix::sys::resource::Resource;
 
 fn print(soft: bool) -> i32 {
     let items = [
-        ("real-time non-blocking time  (microseconds, -R)", Resource::RLIMIT_RTTIME),
-        ("core file size              (blocks, -c)", Resource::RLIMIT_CORE),
-        ("data seg size               (kbytes, -d)", Resource::RLIMIT_DATA),
-        ("scheduling priority                 (-e)", Resource::RLIMIT_NICE),
-        ("file size                   (blocks, -f)", Resource::RLIMIT_FSIZE),
-        ("pending signals                     (-i)", Resource::RLIMIT_SIGPENDING),
-        ("max locked memory           (kbytes, -l)", Resource::RLIMIT_MEMLOCK),
+        ("real-time non-blocking time  ","microseconds, -R", Resource::RLIMIT_RTTIME),
+        ("core file size              ","blocks, -c", Resource::RLIMIT_CORE),
+        ("data seg size               ","kbytes, -d", Resource::RLIMIT_DATA),
+        ("scheduling priority                 ","-e", Resource::RLIMIT_NICE),
+        ("file size                   ","blocks, -f", Resource::RLIMIT_FSIZE),
+        ("pending signals                     ","-i", Resource::RLIMIT_SIGPENDING),
+        ("max locked memory           ","kbytes, -l", Resource::RLIMIT_MEMLOCK),
         //("max memory size             (kbytes, -m)", Resource::RLIMIT_AS),
-        ("open files                          (-n)", Resource::RLIMIT_NOFILE),
+        ("open files                          ","-n", Resource::RLIMIT_NOFILE),
         //("pipe size                (512 bytes, -p)", Resource::RLIMIT_MSGQUEUE),
-        ("POSIX message queues         (bytes, -q)", Resource::RLIMIT_MSGQUEUE),
-        ("real-time priority                  (-r)", Resource::RLIMIT_RTPRIO),
-        ("stack size                  (kbytes, -s)", Resource::RLIMIT_STACK),
-        ("cpu time                   (seconds, -t)", Resource::RLIMIT_RTTIME),
-        //("max user processes                  (-u)", Resource::RLIMIT_RTTIME),
-        ("virtual memory              (kbytes, -v)", Resource::RLIMIT_AS),
-        ("file locks                          (-x)", Resource::RLIMIT_LOCKS),
+        ("POSIX message queues         ","bytes, -q", Resource::RLIMIT_MSGQUEUE),
+        ("real-time priority                  ","-r", Resource::RLIMIT_RTPRIO),
+        ("stack size                  ","kbytes, -s", Resource::RLIMIT_STACK),
+        ("cpu time                   ","seconds, -t", Resource::RLIMIT_RTTIME),
+        //("max user processes                  (-u", Resource::RLIMIT_RTTIME),
+        ("virtual memory              ","kbytes, -v", Resource::RLIMIT_AS),
+        ("file locks                          ","-x", Resource::RLIMIT_LOCKS),
     ];
 
-    for (item, key) in items {
+    for (item, unit, key) in items {
         let (soft_limit, hard_limit) = resource::getrlimit(key).unwrap();
 
         let mut v = if soft { soft_limit } else { hard_limit };
+        let mut infty = nix::sys::resource::RLIM_INFINITY;
 
-        if key == Resource::RLIMIT_MEMLOCK
-        || key == Resource::RLIMIT_STACK {
+        if unit.starts_with("kbytes") {
             v /= 1024;
+            infty /= 1024;
         }
 
-        let s = match v {
-            nix::sys::resource::RLIM_INFINITY => "unlimited",
-            _ => &v.to_string(),
-        };
+        let s = if v == infty { "unlimited" } else { &v.to_string() };
     
-        println!("{} {}", &item, &s);
+        println!("{}({}) {}", &item, &unit, &s);
     }
     0
 }
