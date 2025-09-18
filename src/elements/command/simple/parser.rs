0b10e84f5e547f1e29d064b0d11fa52ac5c606bc
Ryuichi Ueda <ryuichiueda@gmail.com>
2025-09-19 08:02:08 +0900

    Remove new codes

--- src/elements/command/simple/parser.rs
+++ src/elements/command/simple/parser.rs
@@ -21,24 +21,6 @@ impl SimpleCommand {
         }
     }
 
-    pub fn eat_substitution_as_arg(&mut self, feeder: &mut Feeder,core: &mut ShellCore)
-    -> Result<bool, ParseError> {
-        if let Some(s) = Substitution::parse(feeder, core, true)? {
-            self.text += &s.text;
-            self.substitutions_as_args
-                .push(SubsArgType::Subs(Box::new(s)));
-            return Ok(true);
-        }
-
-        if let Some(w) = Word::parse(feeder, core)? {
-            self.text += &w.text;
-            self.substitutions_as_args.push(SubsArgType::Other(w));
-            return Ok(true);
-        }
-
-        Ok(false)
-    }
-
     fn eat_word(&mut self, feeder: &mut Feeder, core: &mut ShellCore)
     -> Result<bool, ParseError> {
         let w = match Word::parse(feeder, core)? {
@@ -46,12 +28,8 @@ impl SimpleCommand {
             _       => return Ok(false),
         };
 
-        if self.words.is_empty() {
-            if utils::reserved(&w.text) {
-                return Ok(false);
-            }
-
-            self.command_name = w.text.clone();
+        if self.words.is_empty() && utils::reserved(&w.text) {
+            return Ok(false);
         }
         self.text += &w.text;
         self.words.push(w);
@@ -72,13 +50,6 @@ impl SimpleCommand {
 
         loop {
             command::eat_redirects(feeder, core, &mut ans.redirects, &mut ans.text)?;
-
-            if core.subst_builtins.contains_key(&ans.command_name) {
-                if ans.eat_substitution_as_arg(feeder, core)? {
-                    continue;
-                }
-            }
-
             if ! ans.eat_word(feeder, core)? {
                 break;
             }
@@ -86,7 +57,6 @@ impl SimpleCommand {
 
         if ans.words.len() + ans.redirects.len() + ans.substitutions.len() > 0 {
             feeder.pop_backup();
-            dbg!("{:?}", &ans);
             Ok(Some(ans))
         }else{
             feeder.rewind();
