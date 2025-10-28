302a90661e5f32a640757872beedfc23b412ba79
Ryuichi Ueda <ryuichiueda@gmail.com>
2025-10-28 10:16:36 +0900

    Erase pub

--- src/elements/command/simple/parser.rs
+++ src/elements/command/simple/parser.rs
@@ -21,7 +21,7 @@ impl SimpleCommand {
         }
     }
 
-    pub fn eat_substitution_as_arg(&mut self, feeder: &mut Feeder,core: &mut ShellCore)
+    fn eat_substitution_as_arg(&mut self, feeder: &mut Feeder,core: &mut ShellCore)
     -> Result<bool, ParseError> {
         if let Some(s) = Substitution::parse(feeder, core, true)? {
             self.text += &s.text;
