c960c8d78b6c748f20601a9c538d30e49ad34e2b
Ryuichi Ueda <ryuichiueda@gmail.com>
2025-11-22 17:44:43 +0900

    Add set_as_single

--- src/core/database/data.rs
+++ src/core/database/data.rs
@@ -17,6 +17,10 @@ pub trait Data {
     fn boxed_clone(&self) -> Box<dyn Data>;
     fn get_print_string(&self) -> String;
 
+    fn set_as_single(&mut self, _: &str, _: &str) -> Result<(), ExecError> {
+        Err(ExecError::Other("Undefined call set_as_single".to_string()))
+    }
+
     fn get_as_single(&mut self) -> Result<String, ExecError> {
         Err(ExecError::Other("not a single variable".to_string()))
     }
