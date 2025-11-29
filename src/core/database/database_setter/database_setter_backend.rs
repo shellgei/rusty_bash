9c64cd32d0008bd35520fe06c2910c50693f5964
Ryuichi Ueda <ryuichiueda@gmail.com>
2025-11-29 18:17:55 +0900

    Refactor

--- src/core/database/database_setter/database_setter_backend.rs
+++ src/core/database/database_setter/database_setter_backend.rs
@@ -20,18 +20,18 @@ impl DataBase {
                 d.set_as_assoc(name, &pos.to_string(), val)
             } else if d.is_single() {
                 let data = d.get_as_single()?;
-                self.set_uninit_entry(layer, name, Some(vec![]), &flags)?;
+                self.set_uninit_array(layer, name, Some(vec![]), &flags)?;
 
                 if !data.is_empty() {
                     self.set_elem(layer, name, 0, &data, flags)?;
                 }
                 self.set_elem(layer, name, pos, val, flags)
             } else {
-                self.set_uninit_entry(layer, name, Some(vec![]), &flags)?;
+                self.set_uninit_array(layer, name, Some(vec![]), &flags)?;
                 self.set_elem(layer, name, pos, val, flags)
             }
         } else {
-            self.set_uninit_entry(layer, name, Some(vec![]), &flags)?;
+            self.set_uninit_array(layer, name, Some(vec![]), &flags)?;
             self.set_elem(layer, name, pos, val, flags)
         }
     }
@@ -50,22 +50,20 @@ impl DataBase {
                 d.append_to_assoc_elem(name, &pos.to_string(), val)
             } else {
                 let data = d.get_as_single()?;
-                self.set_uninit_entry(layer, name, Some(vec![]), "")?;
+                self.set_uninit_array(layer, name, Some(vec![]), "")?;
                 self.append_elem(layer, name, 0, &data)?;
                 self.append_elem(layer, name, pos, val)
             }
         } else {
-            self.set_uninit_entry(layer, name, Some(vec![]), "")?;
+            self.set_uninit_array(layer, name, Some(vec![]), "")?;
             self.set_elem(layer, name, pos, val, "")
         }
     }
 
-    pub fn set_uninit_entry(&mut self, layer: usize,
+    pub fn set_uninit_array(&mut self, layer: usize,
         name: &str, v: Option<Vec<String>>, flags: &str,
     ) -> Result<(), ExecError> {
-        let obj = if flags.contains('n') {
-            Box::new(Uninit::new("n")) as Box::<dyn Data>
-        }else if flags.contains('i') {
+        let obj = if flags.contains('i') {
             Box::new(Uninit::new("ai")) as Box::<dyn Data>
         }else if v.is_none() {
             Box::new(Uninit::new("a")) as Box::<dyn Data>
