9de8b700abcbd2cf17d4d915c47480bd308e6f50
Ryuichi Ueda <ryuichiueda@gmail.com>
2025-12-26 09:19:07 +0900

    Change order

--- src/core/builtins/variable.rs
+++ src/core/builtins/variable.rs
@@ -6,6 +6,21 @@ use crate::utils::arg;
 use crate::elements::substitution::Substitution;
 use crate::error::exec::ExecError;
 
+fn print_args_match(core: &mut ShellCore, args: &[String]) -> i32 {
+    if args.len() <= 1 { 
+        core.db.print_params_and_funcs();
+        return 0;
+    }
+    print_args_matched_params(core, args)
+}
+
+fn print_args_matched_params(core: &mut ShellCore, args: &[String]) -> i32 {
+    let mut names = core.db.get_param_keys();
+    drop_by_args(core, &mut names, args);
+    names.iter().for_each(|n| {print_with_flags(core, n); });
+    0
+}
+
 fn drop_by_args(core: &mut ShellCore, names: &mut Vec<String>, args: &[String]) {
     for flag in ['i', 'a', 'A', 'r', 'x', 'u', 'n', 'l'] {
         let opt = "-".to_owned() + &flag.to_string();
@@ -23,21 +38,6 @@ fn print_with_flags(core: &mut ShellCore, name: &String) {
 
     let value = core.db.get_param(name).unwrap_or("".to_string());
     println!("declare -{options} {name}={value}");
-}
-
-fn print_args_matched_params(core: &mut ShellCore, args: &[String]) -> i32 {
-    let mut names = core.db.get_param_keys();
-    drop_by_args(core, &mut names, args);
-    names.iter().for_each(|n| {print_with_flags(core, n); });
-    0
-}
-
-fn print_args_match(core: &mut ShellCore, args: &[String]) -> i32 {
-    if args.len() <= 1 { 
-        core.db.print_params_and_funcs();
-        return 0;
-    }
-    print_args_matched_params(core, args)
 }
 
 pub fn declare(core: &mut ShellCore, args: &[String],
