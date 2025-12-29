a89799887b25d038b0a78d7d64c86e55fcd64b88
Ryuichi Ueda <ryuichiueda@gmail.com>
2025-12-29 17:10:27 +0900

    Fix

--- src/core/builtins/caller.rs
+++ src/core/builtins/caller.rs
@@ -10,17 +10,11 @@ pub fn caller(core: &mut ShellCore, _: &[String]) -> i32 {
     }
     let functions_len = core.db.index_based_len("FUNCNAME");
 
-    //dbg!("{:?}", &core.db.get_vec_from("FUNCNAME", 0, false));
-    //dbg!("{:?}", &core.db.get_vec_from("BASH_LINENO", 0, false));
     let lineno = core.db.get_elem("BASH_LINENO", "0").unwrap();
     let mut funcname = core.db.get_elem("FUNCNAME", "1").unwrap();
 
     if funcname == "" {
-        if core.db.flags.contains('i') || linenos_len == functions_len {
-            funcname = "NULL".to_string();
-        }else{
-            funcname = "main".to_string();
-        }
+        funcname = "NULL".to_string();
     }else {
         funcname = "main".to_string();
     }
