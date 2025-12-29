e5f04e1e3c471dc5c34a709ad0558a480f628dc3
Ryuichi Ueda <ryuichiueda@gmail.com>
2025-12-29 17:29:26 +0900

    Implement caller <n> roughly

--- src/core/builtins/caller.rs
+++ src/core/builtins/caller.rs
@@ -3,12 +3,11 @@
 
 use crate::ShellCore;
 
-pub fn caller(core: &mut ShellCore, _: &[String]) -> i32 {
+fn caller_no_arg(core: &mut ShellCore) -> i32 {
     let linenos_len = core.db.index_based_len("BASH_LINENO");
     if linenos_len == 0 {
         return 1;
     }
-    let functions_len = core.db.index_based_len("FUNCNAME");
 
     let lineno = core.db.get_elem("BASH_LINENO", "0").unwrap();
     let mut funcname = core.db.get_elem("FUNCNAME", "1").unwrap();
@@ -21,4 +20,37 @@ pub fn caller(core: &mut ShellCore, _: &[String]) -> i32 {
 
     println!("{} {}", &lineno, funcname);
     0
+}
+
+fn caller_arg(core: &mut ShellCore, args: &[String]) -> i32 {
+    let pos = match args[1].parse::<usize>() {
+        Ok(n) => n,
+        _ => return 1,
+    };
+
+    let linenos_len = core.db.index_based_len("BASH_LINENO");
+    if linenos_len == 0 {
+        return 1;
+    }
+    //let functions_len = core.db.index_based_len("FUNCNAME");
+
+
+    let lineno = core.db.get_elem("BASH_LINENO", &pos.to_string()).unwrap();
+    let funcname = core.db.get_elem("FUNCNAME", &(pos+1).to_string()).unwrap();
+
+    let mut script_name = core.script_name.clone();
+    if script_name == "-" {
+        script_name = "main".to_string();
+    }
+
+    println!("{} {} {}", &lineno, funcname, script_name);
+    0
+}
+
+pub fn caller(core: &mut ShellCore, args: &[String]) -> i32 {
+    if args.len() == 1 {
+        caller_no_arg(core)
+    }else{
+        caller_arg(core, args)
+    }
 }
