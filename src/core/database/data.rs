3a4a1d08444e2f42c2f1e88040c9ca5aa4ef9769
Ryuichi Ueda <ryuichiueda@gmail.com>
2026-02-19 16:49:51 +0900

    Generalize a method

--- src/core/database/data.rs
+++ src/core/database/data.rs
@@ -26,8 +26,8 @@ pub trait Data {
         self._get_fmt_string()
     }
 
-    fn set_as_single(&mut self, _: &str, _: &str) -> Result<(), ExecError> {
-        Err(ExecError::Other("Undefined call set_as_single".to_string()))
+    fn set_as_single(&mut self, name: &str, _: &str) -> Result<(), ExecError> {
+        self.readonly_check(name)
     }
 
     fn get_as_single(&mut self) -> Result<String, ExecError> {
