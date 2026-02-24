0db252600cc9609f29bf380d42f588b126002b84
Ryuichi Ueda <ryuichiueda@gmail.com>
2026-02-24 18:04:48 +0900

    Implement enable -n

--- src/core/builtins/enable.rs
+++ src/core/builtins/enable.rs
@@ -13,12 +13,25 @@ fn print_enabled(core: &mut ShellCore) -> i32 {
     0
 }
 
+fn disable(core: &mut ShellCore, commands: &[String]) -> i32 {
+    for com in commands {
+        if let Some(func) = core.builtins.remove(com) {
+            core.disabled_builtins.insert(com.to_string(), func);
+        }else if let Some(func) = core.subst_builtins.remove(com) {
+            core.disabled_subst_builtins.insert(com.to_string(), func);
+        }
+    }
+    0
+}
+
 pub fn enable(core: &mut ShellCore, args: &[String]) -> i32 {
     let args = args.to_owned();
     let args = arg::dissolve_options(&args);
 
     if args.len() < 2 {
         return print_enabled(core);
+    }else if args[1] == "-n" {
+        disable(core, &args[2..]);
     }
 
     0
