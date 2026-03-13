7248f8cee2308a4a463265b6972e607db7bc543c
Ryuichi Ueda <ryuichiueda@gmail.com>
2026-03-13 16:05:20 +0900

    Fix

--- src/core/time_keeper.rs
+++ src/core/time_keeper.rs
@@ -1,10 +1,11 @@
 //SPDXFileCopyrightText: 2025 Ryuichi Ueda ryuichiueda@gmail.com
 //SPDXLicense-Identifier: BSD-3-Clause
 
-use nix::sys::time::{TimeSpec, TimeVal};
+use nix::sys::time::TimeVal;
+use std::time::Duration;
 
 pub struct TimeKeeper {
-    pub real: Option<TimeSpec>,
+    pub real: Option<Duration>,
     pub user: TimeVal,
     pub sys: TimeVal,
 }
