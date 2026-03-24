eb37e276b27a1cf3febe0eaff4ec4111f379456a
Ryuichi Ueda <ryuichiueda@gmail.com>
2026-03-24 14:02:00 +0900

    昔のコメント除去

--- src/elements/pipeline.rs
+++ src/elements/pipeline.rs
@@ -22,7 +22,7 @@ pub struct Pipeline {
 impl Pipeline {
     pub fn exec(&mut self, core: &mut ShellCore, pgid: Pid)
     -> (Vec<Option<Pid>>, bool, Option<ExecError>) {
-        if core.sigint.load(Relaxed) { //以下4行追加
+        if core.sigint.load(Relaxed) {
             core.db.set_param("?", "130", None).unwrap();
             return (vec![], self.exclamation, Some(ExecError::Interrupted));
         }
