06ab69877e348e6048e8097b6fcd4165e1f17fdb
Ryuichi Ueda <ryuichiueda@gmail.com>
2026-04-11 09:07:19 +0900

    Writing

--- src/elements/subword/braced_param.rs
+++ src/elements/subword/braced_param.rs
@@ -2,6 +2,8 @@
 //SPDX-License-Identifier: BSD-3-Clause
 
 use crate::elements::substitution::variable::Variable;
+use crate::error::parse::ParseError;
+use crate::{Feeder, ShellCore};
 use super::Subword;
 
 #[derive(Debug, Clone, Default)]
@@ -11,6 +13,40 @@ pub struct BracedParam {
 }
 
 impl Subword for BracedParam {
-    fn get_text(&self) -> &str {self.text.as_ref()}
-    fn boxed_clone(&self) -> Box<dyn Subword> {Box::new(self.clone())}
+    fn get_text(&self) -> &str {
+        self.text.as_ref()
+    }
+
+    fn boxed_clone(&self) -> Box<dyn Subword> {
+        Box::new(self.clone())
+    }
+}
+
+impl BracedParam {
+    fn eat_param(feeder: &mut Feeder, ans: &mut Self, core: &mut ShellCore) -> bool {
+        let len = feeder.scanner_name(core);
+        if len != 0 {
+            ans.param = Variable::default();
+            ans.param.text = feeder.consume(len);
+            ans.text += &ans.param.text;
+            return true;
+        }
+
+        feeder.starts_with("}")
+    }
+
+    pub fn parse(feeder: &mut Feeder, core: &mut ShellCore)
+                 -> Result<Option<Self>, ParseError> {
+        if !feeder.starts_with("${") {
+            return Ok(None);
+        }
+        let mut ans = Self::default();
+        ans.text += &feeder.consume(2);
+
+        if Self::eat_param(feeder, &mut ans, core) {
+            ans.text += &feeder.consume(1);
+        }
+
+        Ok(Some(ans))
+    }
 }
