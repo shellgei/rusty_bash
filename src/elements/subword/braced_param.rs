4f9b421fa1eaa42eead669d7cda1452c5d879eb5
Ryuichi Ueda <ryuichiueda@gmail.com>
2026-04-11 10:21:43 +0900

    Remove implementation

--- src/elements/subword/braced_param.rs
+++ src/elements/subword/braced_param.rs
@@ -23,30 +23,8 @@ impl Subword for BracedParam {
 }
 
 impl BracedParam {
-    fn eat_param(feeder: &mut Feeder, ans: &mut Self, core: &mut ShellCore) -> bool {
-        let len = feeder.scanner_name(core);
-        if len != 0 {
-            ans.param = Variable::default();
-            ans.param.text = feeder.consume(len);
-            ans.text += &ans.param.text;
-            return true;
-        }
-
-        feeder.starts_with("}")
-    }
-
     pub fn parse(feeder: &mut Feeder, core: &mut ShellCore)
-                 -> Result<Option<Self>, ParseError> {
-        if !feeder.starts_with("${") {
-            return Ok(None);
-        }
-        let mut ans = Self::default();
-        ans.text += &feeder.consume(2);
-
-        if Self::eat_param(feeder, &mut ans, core) {
-            ans.text += &feeder.consume(1);
-        }
-
-        Ok(Some(ans))
+    -> Result<Option<Self>, ParseError> {
+        Ok(None)
     }
 }
