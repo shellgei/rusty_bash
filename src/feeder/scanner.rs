6bcce37cbe39623fb1f6ed4d08334d88af112056
Ryuichi Ueda <ryuichiueda@gmail.com>
2026-05-02 15:29:59 +0900

    Simplify

--- src/feeder/scanner.rs
+++ src/feeder/scanner.rs
@@ -88,9 +88,9 @@ impl Feeder {
         }
     }
 
-    pub fn scanner_special_and_positional_param(&mut self) -> usize {
+    pub fn scanner_special_param(&mut self) -> usize {
         match self.remaining.chars().nth(0) {
-            Some(c) => "$?*@#-!_0123456789".find(c).is_some() as usize,
+            Some(c) => "$?*@#-!_".find(c).is_some() as usize,
             None    => 0,
         }   
     }
