bc4f86d5d9b6964cf9768e563fc0c622bd70bb57
Ryuichi Ueda <ryuichiueda@gmail.com>
2026-05-30 10:01:21 +0900

    Fix

--- src/elements/braced_param_ext/substr.rs
+++ src/elements/braced_param_ext/substr.rs
@@ -3,30 +3,18 @@
 
 use crate::{Feeder, ShellCore};
 use crate::elements::word::{Word, WordMode};
-use crate::error::exec::ExecError;
 use crate::error::parse::ParseError;
 use super::BracedParamExtension;
 
 #[derive(Debug, Clone, Default)]
 pub struct Substr {
     pub text: String,
-    pub offset: Option<Word>, //本来は計算式を入れられる
+    pub offset: Word, //本来は計算式を入れられる
     pub length: Option<Word>, //同上
 }
 
 impl BracedParamExtension for Substr {
     fn get_text(&self) -> String { self.text.clone() }
-
-    fn exec(&mut self, v: &Variable, text: &str,
-            core: &mut ShellCore) -> Result<String, ExecError> {
-        if self.offset.text.is_empty() {
-            if self.length.is_none() {
-                return Err(ExecError::BadSubstitution(self.text.clone()));
-            }
-        }
-        Ok(text.to_string())
-    }
-
     fn boxed_clone(&self) -> Box<dyn BracedParamExtension> { Box::new(self.clone()) }
 }
 
@@ -43,9 +31,9 @@ impl Substr {
         ans.offset = match Word::parse(feeder, core, Some(mode))? {
             Some(w) => {
                 ans.text += &w.text.clone();
-                Some(w)
+                w
             },
-            None => None,
+            None => Word::default(),
         };
 
         dbg!("{:?}", &ans);
