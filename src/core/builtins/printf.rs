bcae159800bfe5a27301a3d33e824bcf99c27d16
Ryuichi Ueda <ryuichiueda@gmail.com>
2026-06-12 18:50:18 +0900

    Add a comment

--- src/core/builtins/printf.rs
+++ src/core/builtins/printf.rs
@@ -387,7 +387,8 @@ fn parse(pattern: &str) -> Vec<PrintfToken> {
     ans
 }
 
-/* TODO: Use [u8] instead of String for any output */
+/* TODO: Use [u8] as the return type instead of String for any output */
+/* or directly output to stdout */
 fn format(pattern: &str, args: &mut Vec<String>) -> Result<String, ExecError> {
     let mut ans = String::new();
 
