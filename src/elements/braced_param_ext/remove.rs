73146f088eadf052c7548f57db493f9bc7dc5fda
Ryuichi Ueda <ryuichiueda@gmail.com>
2026-07-04 08:32:30 +0900

    Fix

--- src/elements/braced_param_ext/remove.rs
+++ src/elements/braced_param_ext/remove.rs
@@ -1,11 +1,32 @@
 //SPDX-FileCopyrightText: 2026 Ryuichi Ueda <ryuichiueda@gmail.com>
 //SPDX-License-Identifier: BSD-3-Clause
 
-use crate::elements::word::Word;
+use crate::{Feeder, ShellCore};
+use crate::elements::word::{Word, WordMode};
+use super::ParseError;
 
 #[derive(Debug, Clone, Default)]
 pub struct Remove {
     pub text: String,
     pub symbol: String,
-    pub pattern: Option<Word>,
+    pub pattern: Word,
+}
+
+impl Remove {
+    pub fn parse(feeder: &mut Feeder, core: &mut ShellCore)
+    -> Result<Option<Self>, ParseError> {
+        let len = feeder.scanner_parameter_remove_symbol();
+        if len == 0 { 
+            return Ok(None);
+        }   
+
+        let mut ans = Remove::default();
+        ans.symbol = feeder.consume(len);
+        ans.text += &ans.symbol.clone();
+
+        let mode = Some(WordMode::PermitAnyUntil(vec!["}".to_string()]));
+        ans.pattern = Word::parse(feeder, core, mode)?.unwrap_or_default();
+        ans.text += &ans.pattern.text.clone();
+        Ok(Some(ans))
+    }
 }
