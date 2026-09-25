620d71518cb6ac57750c56778fdb57324234b082
Ryuichi Ueda <ryuichiueda@gmail.com>
2026-09-25 10:58:01 +0900

    Writing

--- test/test.bash
+++ test/test.bash
@@ -933,10 +933,10 @@ res=$($com <<< 'A= ; echo ${A:+set}' )
 res=$($com <<< 'A=aaa ; echo ${A:+set}' )
 [ "$res" = "set" ] || err $LINENO
 
-#res=$($com <<< 'A=aaa ; echo ${A:+"set
-#ok"}' )
-#[ "$res" = "set
-#ok" ] || err $LINENO
+res=$($com <<< 'A=aaa ; echo ${A:+"set
+ok"}' )
+[ "$res" = "set
+ok" ] || err $LINENO
 
 res=$($com <<< 'echo ${A-abc}' )
 [ "$res" = "abc" ] || err $LINENO
